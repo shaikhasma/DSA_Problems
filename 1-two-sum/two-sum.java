cc19af121df1359bfd754f7eecee21c3dace62ad
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2023-09-01 16:31:51 +0530

    Time: 1 ms (99.42%) | Memory: 44.1 MB (5.74%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,26 @@
+/** Striver SDE Sheet Challenge By Asma 
+Problem : Two Sum 
+Approach : Using Hashing
+*/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] sumIndex = new int[2];
+        for(int index = 0; index < nums.length; index++){
+            int difference = target - nums[index];
+            if(map.containsKey(difference)){
+                sumIndex[0] = map.get(difference);
+                sumIndex[1] = index;
+                return sumIndex;
+            }else{
+                map.put(nums[index], index);
+            }
+        }
+
+        return sumIndex;
+    }
+}
+/**
+TC : 0(N)
+SC : 0(N)
+ */
