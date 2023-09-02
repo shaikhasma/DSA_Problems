5e04e400791d5ffb3807af61f881168f6c45b6d7
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2023-09-02 18:48:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (12.66%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,19 +1,24 @@
 /* 
-Search in 2 D Matrix Brute force By Asma 
-T.C - 0(NM)
+Search in 2 D Matrix selecting / skiping row/col  By Asma 
+T.C - 0(N)
 S.C - 0(1)
 */
 
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-       for(int row = 0; row < matrix.length; row++){
-           for(int col = 0; col < matrix[0].length; col++){
-              if(target == matrix[row][col])
-                return true;
-           }
+       int row = 0;
+       int col = matrix[0].length - 1;
+
+       while(row < matrix.length && col >= 0){
+           if(matrix[row][col] == target)
+              return true;
+           
+           if( matrix[row][col] > target)
+               col--;  
+           else if(matrix[row][col] < target)
+               row++;
        }
 
        return false;
-        
     }        
 }
