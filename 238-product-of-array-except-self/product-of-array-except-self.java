071b870840c69ab3eee85ef87478fce65e6d4d92
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2023-09-03 09:03:20 +0530

    Time: 2 ms (61.55%) | Memory: 53.5 MB (27.90%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,23 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
      int[] left = new int[nums.length];
-     int[] right = new int[nums.length];
-     int[] res = new int[nums.length];
 
-     left[0] = nums[0];
-     right[nums.length - 1] = nums[nums.length - 1];
-     for(int index = 1; index < nums.length; index++){
-      left [index] = left [index - 1] * nums[index];
+    left[0] = nums[0];
+     
+     for(int index = 1; index < nums.length ; index++){
+         left[index] = left[index - 1] * nums[index];
      }
-
-     for(int index = nums.length - 2; index >= 0 ; index--){
-         right[index] = right[index + 1] * nums[index];
+     
+     left [nums.length - 1] = left[nums.length - 2]; 
+     int prod = 1;
+     for(int index = nums.length - 1 ; index > 0; index--){
+         left [index] = left[index - 1] * prod;
+         prod = prod * nums[index];
      }
-    res[0] = right[1];
-    res[nums.length - 1] = left[nums.length - 2];
-     for(int index = 1; index <= nums.length - 2; index++){
-         res[index] = left[index - 1] * right[index + 1];
-     }
-    return res;
+     left[0] = prod;
+     return left;
     }
 }
+
