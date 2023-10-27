d85b93175c85ac53426caa14f06042fd626ed558
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2023-10-27 22:03:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (55.10%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,53 @@
+/* Approach - find break point + find closed greater of break point swap + sort remaining
+TC - 0(n) find break point
+     0(n) find closest greater
+     0(n) reverse array
+     0(3N)
+     ~ 0(N)
+
+SC - 0(1)
+*/
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int breakPoint = -1;
+       //Step 1 - find the break point
+        for(int index = nums.length - 2; index >=0; index--){
+            if(nums[index] < nums[index + 1]){
+                breakPoint = index;
+                break;
+            }
+        }
+
+        //Step 2 - if dont have any break point
+        if(breakPoint == -1){
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+
+        //Step 3 - find the closet next greater then breakPoint & swap
+        for(int index = nums.length - 1; index >= 0; index--){
+            if(nums[breakPoint] < nums[index]){
+                swap(nums, breakPoint, index);
+                break;
+            }
+        }
+
+        //step 4 - Sort the remaining elements( breakPoint + 1 to end)
+        reverse(nums, breakPoint + 1, nums.length -1 );
+    }
+    
+    void swap(int[] A, int i, int j){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+    
+    void reverse(int[] A, int i, int j){
+        while(i < j){
+            swap(A, i, j);
+            i++;
+            j--;
+        }
+    }
+}
