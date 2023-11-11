b1729a4f7a585e0c75b8d0136b4c9c3a3d06cd8d
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2023-11-11 06:55:22 +0530

    Time: 86 ms (11.28%) | Memory: 44.2 MB (14.79%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,22 +1,40 @@
+
+/* Approach 1 - Check all substring + hashing
+
+1. Traverse all substring 
+2. once we find duplicate  calculate leng from start to end and stop 
+  and break that substring there it self
+3. Maintain max len
+4. Add character ( if character is already added it wont add again so add directly)
+TC- 0(N^2)
+SC - 0(N)
+*/
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        int len = 0;
-        int maxLen = 0;
-        int left = 0;
-        for(int index = 0; index < s.length(); index++){
-           char ch= s.charAt(index);
-           
-           if(map.containsKey(ch)){
-              left = Math.max(map.get(s.charAt(index)) + 1, left);
-           }
-          
-            map.put(ch,index);
-            len = index - left + 1;
-            maxLen = Math.max(len, maxLen);   
-        }
+       if(s.length() == 0|| s.length() == 1)
+          return s.length();
 
-        return maxLen;
 
+       int len = 0;
+       int maxLen = 0;
+       for(int left = 0; left < s.length(); left++){
+          HashSet<Character> set = new HashSet<Character>();
+          len = 0; 
+          for(int right = left; right < s.length(); right++){
+            char ch = s.charAt(right);
+         
+              if(set.contains(ch)){
+                  // we are at duplicate character that why not right - left + 1
+                  break;
+              }
+
+              //if will never add repeat char
+              set.add(ch);
+              len++; 
+              maxLen = Math.max(len, maxLen);
+          }
+       }
+       return maxLen;
     }
 }
