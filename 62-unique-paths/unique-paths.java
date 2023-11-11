b8352886d6649389b92edbfaa4dbae1986cff8dd
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2023-11-11 18:50:12 +0530

    Time: 0 ms (100.00%) | Memory: 39 MB (74.82%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,29 +1,15 @@
-/* Approach - 2 DP Memoization 
-TC-0(n*m)
-SC- 0(n*m)
+/* Approach - 2 NCR formula 
+TC- O(n-1) or  O(m-1) depending on formula which we are using
+SC- 0(1)
 */
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for(int[] temp : dp){
-            Arrays.fill(temp,-1);
-        }
-      
-        return countPaths(0,0,m,n,dp);
-    }
-
-    int countPaths(int rowIndex, int colIndex,int row, int col,int[][] dp){
-        if(rowIndex == row - 1 && colIndex == col - 1)
-            return 1;
-        if(rowIndex >= row || colIndex >= col)
-            return 0;
-        if(dp[rowIndex][colIndex] != -1)
-          return dp[rowIndex][colIndex];
-
-        return dp[rowIndex][colIndex] = 
-               (countPaths(rowIndex + 1, colIndex, row, col, dp) 
-                  + countPaths(rowIndex, colIndex + 1, row, col,dp) );
-                
-        
+       int N = n + m - 2;
+       int r = m - 1; 
+       double res = 1;
+            
+        for (int i = 1; i <= r; i++)
+            res = res * (N - r + i) / i;
+        return (int)res;
     }
 }
