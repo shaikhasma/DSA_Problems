e90dfa39466f8fb047f9007d3a57a9bc3e68f161
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2023-11-16 10:46:52 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (38.15%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,31 +1,34 @@
+/* Approach 3 Three Pointer Approach
+TC - 0(n)
+SC- 0(1)
+1. start left & mid from 0
+2. start right from n-1
+3. move mid 
+-- if we found 0 swap with left  move left , mid
+-- if we found 2 swap with right move right -- 
+-- if we found 1 dont do any thing move mid
+*/
 class Solution {
     public void sortColors(int[] nums) {
-        int left = 0;
-        int mid = 0;
-        int right = nums.length-1;
-
-        while(mid <= right){
-             if(nums[mid] == 0){
-                swap(nums, left, mid);
-                left++;
-                mid++;
-            }else if(nums[mid] == 2){
-                swap(nums, right, mid);
-                right--;
-            }else{
-                mid++;
-            }    
-        }
-
-        for(int index = 0; index < nums.length ; index++){
-            System.out.print( nums[index]+ " ");
-        }
+      int left = 0;
+      int mid = 0;
+      int right = nums.length - 1;
+      while(mid <= right){
+          if(nums[mid] == 0){
+              swap(nums, mid, left);
+              mid++;
+              left++;
+          }else if(nums[mid] == 2){
+              swap(nums,mid, right);
+              right--;
+          }else{
+              mid++;
+          }
+      }
     }
-
-    void swap(int[] num, int i , int j){
-        int temp = num[i];
-        num[i] = num[j];
-        num[j] = temp;
+    void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
-       
 }
