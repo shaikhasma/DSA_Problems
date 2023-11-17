0d1e341d4f14d9b1d321f48b22f8c1d967dcf5f6
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2023-11-17 08:37:15 +0530

    Time: 1 ms (99.99%) | Memory: 60.2 MB (7.53%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,28 +1,21 @@
 /*
- Approach 4 - Kadane's Algorithm'  
+ Approach 3 - Kadane's Algorithm 
  TC- 0(n)
  SC - 0(1)
 */
 class Solution {
     public int maxSubArray(int[] nums) {
-    if(nums.length == 1)
-        return nums[0];
-    //Step 1 : Take two sum and max sum
-    //         sum = 0 & maxSum = -INFINITY
-
-    int sum = 0;
-    int maxSum = Integer.MIN_VALUE;
-    //Step 2 : traverse 0 to n-1 make  add every element into sum 
-    for(int index = 0 ; index < nums.length; index++){
-        sum = sum + nums[index];
-        
-        //Step 3 : if sum became < 0 it wont give max sum so make it zero for next calculation
-        maxSum = Math.max(maxSum, sum);
-
-        if(sum < 0)
-          sum = 0;
-    }
-
+     if(nums.length == 1)
+       return nums[0];
+     int sum = 0;
+     int maxSum = Integer.MIN_VALUE;
+     for(int index = 0; index < nums.length ; index++){
+         sum = sum + nums[index];
+         maxSum = Math.max(maxSum, sum);   
+         
+         if(sum < 0)
+            sum = 0;
+     }
     return maxSum;
     }
 }
