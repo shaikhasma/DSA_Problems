1c83107e1fe57b5f4b6414a2c762af78ce6aa56d
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2023-12-24 20:26:24 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (6.56%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    final static char ONE = '1';
+    final static char ZERO = '0';
+    public static int minOperations(String s) {
+        char firstChar = s.charAt(0);
+        
+
+        int countWithoutFlipStart = countFlip(s, firstChar);
+        int countWithFlipStart = countFlip(s, firstChar == ZERO ? ONE : ZERO) + 1;
+
+        return Math.min(countWithoutFlipStart, countWithFlipStart);
+    }
+
+   static int countFlip(String s, char prevChar){
+        int totalFlip = 0;
+
+        for(int index = 1; index < s.length(); index++){
+            char current = s.charAt(index);
+            if(current == prevChar){
+                totalFlip++;
+                prevChar = (prevChar == ZERO ? ONE: ZERO ); 
+            }else{
+                prevChar = current;
+            }
+        }
+        return totalFlip;
+    }
+
+}
