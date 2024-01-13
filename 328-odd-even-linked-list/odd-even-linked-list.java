797bbc1cc4c8a0b8b66fab733abd7dc0b00d7422
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-01-13 12:06:49 +0530

    Time: 2 ms (13.21%) | Memory: 44.2 MB (42.79%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -8,23 +8,58 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ /*Approach - 1 using two list and join those two list
+ 1. create new list which hold odd nodes
+ 2. create another list which hold even nodes
+ 3. Attach 2nd list to last of 1st list 
+ 4. return head
+ TC - 0(n) + 0(n)
+ SC - 0(n) 
+
+ */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if(head == null || head.next == null) return head;
-        
-        ListNode temp1 = head;
-        ListNode evenListHead = head.next;
+        ArrayList<Integer> arr = new ArrayList<>();
+        ListNode temp = head;
 
-        ListNode temp2 = evenListHead;
-        
-        while(temp2 != null && temp2.next != null ){
-            temp1.next = temp2.next;
-            temp1 = temp1.next;
-
-            temp2.next = temp1.next;
-            temp2 = temp2.next;
+        if(head == null || head.next == null)
+            return head;
+        fillOddNodes(head, arr);
+        fillEvenNodes(head, arr);
+      
+        //copy values from temp llist to original linked list
+        temp = head;
+        for(int index = 0; index < arr.size(); index++){
+            temp.val = arr.get(index);
+            temp = temp.next;
         }
-        temp1.next = evenListHead;
+
         return head;
+    }
+
+   void fillOddNodes(ListNode head, ArrayList<Integer> list){
+      // starting copying odd list nodes 
+       ListNode temp = head;
+        while(temp != null && temp.next != null){
+            list.add(temp.val);
+            temp = temp.next.next;
+        } 
+
+        // To add the last element if it's odd length
+        if(temp != null)
+            list.add(temp.val);
+        
+   }
+    void fillEvenNodes(ListNode head, ArrayList<Integer> list){
+        // starting copying even list nodes 
+        ListNode temp = head.next;
+        while(temp != null && temp.next != null){
+            list.add(temp.val);
+            temp = temp.next.next;
+        }
+
+        if(temp != null)
+            list.add(temp.val);
+    
     }
 }
