5db4d4af27bd400eb91ad3c43e95977f670eaa1f
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-01-25 05:19:56 +0530

    Time: 9 ms (92.63%) | Memory: 56.8 MB (37.06%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -25,39 +25,34 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-     if(head == null || head.next == null) 
+    if(head == null || head.next == null){
         return head;
+    }
 
-     //Step 1 : find the middle 
-     ListNode middle = findMiddle(head);
      ListNode leftHead = head;
-     ListNode rightHead = middle.next;
-     middle.next = null;
-
-    // step 2: sort each half
+     ListNode middleNode = findMiddle(head);
+     ListNode rightHead = middleNode.next;
+     middleNode.next = null; // cut references from second half
      leftHead = sortList(leftHead);
      rightHead = sortList(rightHead);
 
-    // step 3 : merge both sorted left & right half of list
-     return mergeSortedList(leftHead, rightHead);
-   
+     return mergeList(leftHead, rightHead);
     }
-
+    
     ListNode findMiddle(ListNode head){
-        if(head == null || head.next == null) return head;
+        if(head == null || head.next == null)
+         return head;
+
         ListNode slowPointer = head;
         ListNode fastPointer = head.next;
-        
         while(fastPointer != null && fastPointer.next != null){
             slowPointer = slowPointer.next;
             fastPointer = fastPointer.next.next;
         }
-        
         return slowPointer;
     }
 
-    ListNode mergeSortedList(ListNode head1, ListNode head2){
-    
+    ListNode mergeList(ListNode head1, ListNode head2){
         ListNode dummyHead = new ListNode(-1);
         ListNode dummyTemp = dummyHead;
         ListNode temp1 = head1;
@@ -69,19 +64,17 @@ class Solution {
                 temp2 = temp2.next;
             }else{
                 dummyTemp.next = temp1;
-                temp1= temp1.next;
+                temp1 = temp1.next;
             }
+
             dummyTemp = dummyTemp.next;
         }
-
         if(temp1 != null)
-            dummyTemp.next = temp1;
-        
-        if(temp2 != null)
-            dummyTemp.next = temp2;
+          dummyTemp.next =  temp1;
+        else
+          dummyTemp.next = temp2;
 
-
-    return dummyHead.next;
+        return dummyHead.next;
     }
 
 }
