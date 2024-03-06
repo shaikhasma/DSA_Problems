f264dd5b7afbc648e3a564ebdf48438ba45d288f
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-03-06 15:00:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (59.64%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -9,6 +9,25 @@
  * }
  */
  /*
+ Approach 1- Two pointer approach
+ 1. traverse 
+    prev stand to head
+    current stand to head.next
+2. if both are equal then 
+    - only move current
+    if both are different then
+    - move prev to current
+    - current move ahead by one step
+
+3. repeate step 1 & 2 till current not null
+
+4. if prev next is not null means prev is not last node then
+   - attach prev next equal to null as current move ahead and gap is there in between pre and current 
+     there are duplicates node 
+
+
+TC - 0(N)
+TC - 0(1)
   1 1 2 3 3
         p   c 
  */
@@ -29,7 +48,7 @@ class Solution {
 
         }
 
-        if(prev.next != null )
+        if(prev.next != null)
           prev.next = null;
         
      
