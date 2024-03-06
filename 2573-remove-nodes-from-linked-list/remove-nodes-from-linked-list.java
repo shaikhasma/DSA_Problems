aee962747b381ae6883757420923f97b95ccb5b2
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-03-06 06:33:12 +0530

    Time: 6 ms (94.37%) | Memory: 69.1 MB (45.60%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -9,20 +9,61 @@
  * }
  */
 
- /*
- Approach 1 - recursion
+  /*
+ Approach 1 - reverse List + cut dettach smaller nodes and again revere
+ 1. reverse LL
+ 2. traverse LL
+    prev = firstNode
+    current = firstNode.next
 
- TC - 0(n)
+    if current node is small then pre 
+      move current
+    if current node is greate or equal then 
+      attach prev to current
+      move prev & current bot
+
+3. reverse LL
+5. return head
+
+ TC - N + N + N
    - 0(n)
- S.C - 0(N) auxilary space
+ S.C - 0(1)
    8 3 13 2 5
      c P
  */
 class Solution {
     public ListNode removeNodes(ListNode head) {
  
-    if (head == null) return null;
-            head.next = removeNodes(head.next);
-            return head.next != null && head.val < head.next.val ? head.next : head;
-    }   
+        ListNode lastNode = reverseLL(head);
+        ListNode currentNode = lastNode.next;
+        ListNode prevNode = lastNode;
+
+        while(currentNode != null){
+            if(prevNode.val <= currentNode.val){
+             prevNode.next = currentNode;
+             prevNode = prevNode.next;
+            }
+            currentNode = currentNode.next;
+        }
+
+        prevNode.next = null;
+        return reverseLL(lastNode);
+    }
+
+    ListNode reverseLL(ListNode head){
+       if(head == null)  return head;
+
+       ListNode current = head;
+       ListNode prev = null;
+
+       while(current != null){
+           ListNode next = current.next;
+           current.next = prev;
+
+           prev = current;
+           current = next;
+       }
+
+       return prev;
+    }
 }
