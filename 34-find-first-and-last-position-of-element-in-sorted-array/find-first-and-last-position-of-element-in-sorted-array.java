727a3f99e37d457fbaae592bcb4b5b500a2eff00
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-03-30 18:23:45 +0530

    Time: 1 ms (23.43%) | Memory: 45.5 MB (88.35%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,55 +1,23 @@
-/* Approach - using Lower Bound & Upper Bound
-TC - 0(log N) + 0(log N) = 0(log N)
+/* Approach - Brute Force
+TC - 0(N)
 SC - 0(1)
 */
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums.length == 0) return new int[]{ -1, -1};
-        if(nums.length == 1 && nums[0] == target) return new int[]{0 , 0};
-        int firstPosition = getLowerBound(nums,target);
+        int firstIndex = -1;
+        int lastIndex = -1;
 
-       if( firstPosition == nums.length || nums[firstPosition] != target ) 
-               return new int[]{ -1, -1};   
-       int lastPosition = getUpperBound(nums,target);
-
-        return new int[]{firstPosition, lastPosition - 1};
-    }
-
-    int getLowerBound(int[] nums, int target){
-        int low = 0;
-        int high = nums.length - 1;
-        int lowerBound = nums.length;
-        while(low <= high){
-            int mid = high - ( high - low) / 2;
-            if(nums[mid] == target)
-                lowerBound = mid;
-            
-            if(nums[mid] >= target)
-                high = mid - 1;
-            else
-                low = mid + 1;
-
-        }
-
-        return lowerBound;
-    }
-
-    int getUpperBound(int[] nums, int target){
-        int low = 0;
-        int high = nums.length - 1;
-        int upperBound = nums.length;
-
-        while(low <= high){
-            int mid = high - (high - low) /2;
-
-            if(nums[mid] > target){
-              high = mid - 1;
-              upperBound = mid;
+        for(int index = 0; index < nums.length ; index ++){
+            if(target == nums[index]){
+                if(firstIndex == - 1){
+                    firstIndex = index;
+                    lastIndex = index;
+                }
+                else{
+                    lastIndex = index;
+                }
             }
-            else 
-              low = mid + 1;
         }
-
-        return upperBound;
+        return new int[] {firstIndex , lastIndex};
     }
 }
