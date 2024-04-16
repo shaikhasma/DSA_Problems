4e95f39cc29d8c25c5a4447fd0f055623ac1404d
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-04-16 21:42:49 +0530

    Time: 5 ms (87.41%) | Memory: 44.1 MB (80.98%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,42 +1,22 @@
-
-/* Approach 2 - Sliding window + hashing
-
-1. start left right both from 0 
-2. move right by one step every time check 
- - if righ char present Check 
-  --- if prevIndex is not in range dont move left 
-      else move  left to prevIndex of character + 1
- - add right character into map with index
-
- - Calculate length ( right - left + 1)
-
-TC- 0(N)
-SC - 0(N) 
-
-*/
-
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-      if(s.length() == 0 || s.length() == 0){
-          return s.length();
-      }
+        HashMap<Character,Integer> map = new HashMap<>();
+        int len = 0;
+        int maxLen = 0;
+        int left = 0;
+        for(int index = 0; index < s.length(); index++){
+           char ch= s.charAt(index);
+           
+           if(map.containsKey(ch)){
+              left = Math.max(map.get(s.charAt(index)) + 1, left);
+           }
+          
+            map.put(ch,index);
+            len = index - left + 1;
+            maxLen = Math.max(len, maxLen);   
+        }
 
-     
-      int maxLen = 0;
-      for(int left = 0; left < s.length(); left++){
-          HashSet<Character> set = new HashSet<>();
-          int len = 0;
-          for(int right = left ; right< s.length(); right++){
-              
-                 if(set.contains(s.charAt(right))){
-                     break;
-                 }
-    
-          set.add(s.charAt(right));
-          len++;
-          maxLen = Math.max(maxLen,len);
-          }
-      }
-      return maxLen;
+        return maxLen;
+
     }
 }
