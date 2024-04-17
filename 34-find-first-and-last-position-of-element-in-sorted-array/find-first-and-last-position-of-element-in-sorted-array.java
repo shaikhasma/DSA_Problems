bb4319c3dbd1de2f908f007d111b9b0b222b18e3
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-04-17 19:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (61.03%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,76 +1,66 @@
-/* Approach - Brute Force
-TC - 0(N)
-SC - 0(1)
-*/
-/* Approach - 2 Using binary search
-
-1. find firstIndex using binary search
--- if we find exact target update index 
--- if we find >= target move left to get first index 
-do this till low <= high
-
-2. find lastIndex using binary search
-
--- if we find exact target update index 
--- if we find <= target move right to get last index 
-do this till low <= high
-TC - 0(logN) + 0(logN)
-SC - 0(1)
-
+/*
+[5,7,7,8,8,10]
+ 0 1 2 3 4 5
+       
 */
 
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-      int firstIndex = findFirstIndex(nums, target);
-      int lastIndex = findLastIndex (nums, target);
+    public int[] searchRange(int[] A, int target) {
+        int[] ans = new int[2];
+        ans[0] = firstPosition(A, target);
+        ans[1] = lastPosition(A, target);
+        return ans;
 
-      return new int[] {firstIndex, lastIndex};
+        
     }
 
-    int findFirstIndex(int[] nums, int target){
-        int low = 0 ;
-        int high = nums.length - 1;
-        int firstIndex = -1;
+    int firstPosition(int[] A, int target){
+        int low = 0;
+        int high = A.length - 1;
+       
+        
+        int firstPosition = -1;
         while(low <= high){
-            int mid = (high + low) / 2;
-            //updating firstIndex  every time my moving left 
-            if(nums[mid] == target){
-                firstIndex = mid;
-            }
-
-            if(nums[mid] >= target){
+            int mid = high - ( high - low) / 2;
+            
+            if(A[mid] == target){
+                firstPosition = mid;
                 high = mid - 1;
             }
+            else if( A[mid] < target){
+               low = mid + 1;
+               
+            }
             else{
-                low = mid + 1;
+                high = mid - 1;
             }
         }
 
-        return firstIndex;
+        return firstPosition;
     }
 
-    int findLastIndex (int[] nums, int target){
+    int lastPosition(int[] A, int target){
         int low = 0;
-        int high = nums.length - 1;
-        int lastIndex = -1;
-
+        int high = A.length - 1;
+       
+        
+        int lastPosition = -1;
         while(low <= high){
-          int mid = (high + low ) / 2;
-          
-          //updating last index everytime by moving right side
-          if(nums[mid] == target){
-            lastIndex = mid;
-          }
-          
-          if(nums[mid] <= target){
-            low = mid + 1;
-          }
-          else{
-            high = mid - 1;
-          }
+            int mid = high - ( high - low) / 2;
+            
+            if(A[mid] == target){
+                lastPosition = mid;
+                low = mid + 1;
+            }
+            else if( A[mid] < target){
+               low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
         }
 
-        return lastIndex;
-
+        return lastPosition;
     }
+
 }
