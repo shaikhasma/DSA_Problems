0d4cd3789de52e355d0764e69a5f0385cb05e47a
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-04-23 21:02:07 +0530

    Time: 27 ms (64.62%) | Memory: 58 MB (20.75%) - LeetSync

---
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -0,0 +1,54 @@
+/*
+(1) A tree is an undirected graph in which any two vertices are
+connected by exactly one path.
+
+(2) Any connected graph who has n nodes with n-1 edges is a tree.
+
+(3) The degree of a vertex of a graph is the number of
+edges incident to the vertex.
+
+(4) A leaf is a vertex of degree 1. An internal vertex is a vertex of
+degree at least 2.
+
+(5) A path graph is a tree with two or more vertices that is not
+branched at all.
+
+(6) A tree is called a rooted tree if one vertex has been designated
+the root.
+
+(7) The height of a rooted tree is the number of edges on the longest
+downward path between root and a leaf.
+*/
+
+class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        if (n == 1) return 
+            Collections.singletonList(0);
+
+        List<Set<Integer>> adj = new ArrayList<>(n);
+        for (int i = 0; i < n; ++i) 
+            adj.add(new HashSet<>());
+            
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        List<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; ++i)
+            if (adj.get(i).size() == 1) leaves.add(i);
+
+        while (n > 2) {
+            n -= leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+            for (int i : leaves) {
+                int j = adj.get(i).iterator().next();
+                adj.get(j).remove(i);
+                if (adj.get(j).size() == 1) newLeaves.add(j);
+            }
+            leaves = newLeaves;
+        }
+
+    return leaves;
+    }
+}
