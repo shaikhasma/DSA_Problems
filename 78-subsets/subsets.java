1dbf53f945be03a3bfb114fb47ad7ef0e65df2f4
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-04-23 20:52:03 +0530

    Time: 1 ms (60.52%) | Memory: 42.7 MB (28.61%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,34 @@
+/* Approach - Recursion + Backtracking
+TC: O(n * 2^n)
+SC: O(n * 2^n)
+*/
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        if(nums==null || nums.length==0)   
+           return list;
+         
+        subsets(nums, list, new ArrayList<Integer>(), 0); 
+        
+        return list;
+    }
+
+    private void subsets(int[] nums,  List<List<Integer>> list , ArrayList<Integer> temp, int index) {
+        // base condition
+        if(index >= nums.length) {
+            list.add(new ArrayList<>(temp));
+            return;
+        }
+        
+        // main logic
+		// case 1 : we pick the element
+        temp.add(nums[index]);
+        subsets(nums, list, temp, index+1); // move ahead
+        temp.remove(temp.size() - 1);
+        
+		// case 2 : we don't pick the element ( notice,
+        // we did not add the current element in our temporary list
+        subsets(nums, list, temp, index+1); // move ahead
+
+    }
+}
