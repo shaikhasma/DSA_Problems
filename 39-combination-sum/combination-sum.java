0bbebcefb0b83d2c63095ad565d4639ae99cf9d8
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-04-26 21:59:14 +0530

    Time: 2 ms (83.97%) | Memory: 44.7 MB (22.26%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,27 +1,25 @@
 /*
    Approach - 1 Recursion + Backtraking
-   pick
-   -----
-   1. Pick the element when element is less than target
-     -- target - picked element
-   2. call function recursively with the same element to check possiblity of pick again or not
-   3. here if we come back from recurive call 
-      -- remove current element as next step we are going to exclude or not pick current element 
-   
-   Not pick
-   ---------
-   4. if we cant pick we take other option not to pick element 
-     -- call function recursively with the next element without updating target as we are not picking current element
+   Start with 0 index (index )
+
+   1 Traverse i from index to last index
+
+   2. Pick the element when element is less than target
+    -- target - picked element or reduce target by picked element
+
+   3. call function recursively with the same element to check possiblity of pick again or not
+
+   4. here if we come back from recurive call 
+    -- remove current element as next step we are going to exclude or not pick current element 
+
+  Repeate all 2 3 4 steps till i <= last index 
 
 
    Base Case 
    1. - While performing any option if we find target before reaching to last index 
       - fill temp list of comibination into ans & return back 
-   
-   2. If we reach to last index & unfortunetly we dont found target 
-      - simply return back
 
-      TC - 0(2^t)* k 
+      TC - 0(2^t) * k 
           -  where t is target as we can pick same element multiple time we can pick 1 10 time if target is 10
           - k is avg length of every generated compination which we fill to ans list which will take linear time not constant
 
@@ -31,30 +29,27 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
+         Arrays.sort(candidates);
         solve(candidates, 0,target, new ArrayList<Integer>(), ans);
         return ans;
     }
     
     void solve(int[] A, int index, int target, List<Integer> list, List<List<Integer>> ans){
+
         //if before reaching to end target sum found
         if(target == 0){
             ans.add(new ArrayList<>(list));
             return;
         }
-        // we reach to last if in case we find target fill to ans list & go back 
-        //else directly return back 
-        if (index == A.length ){
-            return;
-        }
-         //include
-        if (A[index] <= target) {
-            list.add(A[index]);
+      
+         for(int i = index; i < A.length; i++){
+            if(A[i] > target) 
+               break;
 
-            solve(A,index, target - A[index], list, ans);
-           
+            list.add(A[i]);
+            solve(A, i , target - A[i], list, ans);
+            
             list.remove(list.size() - 1);
-        }
-        //exclude
-        solve(A, index + 1, target, list, ans);
+         }
     }
 }
