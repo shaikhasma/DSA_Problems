0b1fee2909083d1e70f9f74bf762196017448096
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-05-01 22:09:01 +0530

    Time: 1 ms (95.42%) | Memory: 44.1 MB (84.72%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -4,32 +4,33 @@ SC - 0(n) + 0(n) + 0(n) temp + freq + stack auxilliary space
 */
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-    
-        List<List<Integer>> ans = new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
+     List<List<Integer>> list = new ArrayList<>();
+     boolean[] visited = new boolean[nums.length];
 
-        solve(nums, visited, new ArrayList<>(), ans);
-
-        return ans; 
+     solve(nums, visited, new ArrayList<Integer>(), list);
+     return list; 
     }
 
     void solve(int[] arr, boolean[] visited, ArrayList<Integer> temp, List<List<Integer>> ans ){
-        if(temp.size() == arr.length){
-            ans.add(new ArrayList<>(temp));
-            return;
-        }
+       
+       if(temp.size() == arr.length){
+           ans.add(new ArrayList<>(temp));
+           return;
+       }
 
-        for(int index = 0; index < arr.length; index++){
+       for(int index = 0; index < arr.length ; index++){
+            
             if(!visited[index]){
-                visited[index] = true;
                 temp.add(arr[index]);
+                visited[index] = true;
                 
                 solve(arr, visited, temp, ans);
-
-                temp.remove(temp.size() - 1);
+                
                 visited[index] = false;
+                temp.remove(temp.size() - 1);
             }
-        }
+
+       }
     
     }
 }
