edf095cac3b957fdcbcb630202fec2aa996d3a7c
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-05-02 14:40:41 +0530

    Time: 5 ms (84.11%) | Memory: 44.8 MB (59.62%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
@@ -1,33 +1,50 @@
-/* Approach - Sort +  Binary Search
+/* Approach - Sort +  Two pointer appraoch
+- Sort array
+-- start left = 0 right -= n- 1
 
+-- if sum of left + right elment is zero means both same number with - & + signs
+    keep track of max number with right 
+    move left++
+    move right--
 
- - Traverse 
-   maintain max
+-- if sum < 0 means left is greater and right is smaller with + ve 
+  move left++ to match right number
 
- TC- 0(N) + 0(N)
- Sc- (N)
+-- sum > 0 
+   move right -- to match left number
+
+ [-1,2,-
+   - Traverse 
+   
+
+ TC- 0(NlogN)
+ Sc- 0(1)
 */
 class Solution {
     public int findMaxK(int[] nums) {
        
-       HashSet<Integer> set = new HashSet<Integer>();
-       int max = Integer.MIN_VALUE;
-       int ans = -1;
+      Arrays.sort(nums);
 
-       for(int index = 0; index < nums.length; index++){
-         if(nums[index] < 0)
-           set.add(nums[index]);
+      int left = 0;
+      int right = nums.length - 1;
+      int max = Integer.MIN_VALUE;
+      
+       while(left < right ){
+           int sum = nums[left] + nums[right];
+            if( sum == 0){
+                max = Math.max(max, nums[right]);
+                left++;
+                right--;
+            }
+            else if( sum < 0 ){
+            left++;
+            }
+            else{
+                right--;
+            }
        }
-
-       for(int index = 0 ; index < nums.length; index++){
-    
-         if(nums[index] > max &&  nums[index] > 0 && set.contains(-nums[index])){
-                ans = nums[index] ;
-                max = nums[index];
-         }
-       }
-        
-       return ans;
+        // if we dont find max then return -1 
+       return max != Integer.MIN_VALUE ? max : -1;
        
     }
 }
