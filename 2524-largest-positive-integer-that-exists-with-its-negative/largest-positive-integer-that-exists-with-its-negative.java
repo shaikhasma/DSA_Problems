68b69a00851de7782d8b67e303f1c66939e18ee1
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-05-02 14:17:44 +0530

    Time: 3 ms (92.86%) | Memory: 44.2 MB (97.67%) - LeetSync

---
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
@@ -0,0 +1,32 @@
+/*
+ using hashing 
+ put all numbers in -ve numbers in hashmap
+ while traversing 0 -n find max and check same -ve number is present or not using abs check
+
+ TC- 0(N)
+ Sc- (N)
+*/
+class Solution {
+    public int findMaxK(int[] nums) {
+       
+       HashSet<Integer> set = new HashSet<Integer>();
+       int max = Integer.MIN_VALUE;
+       int ans = -1;
+
+       for(int index = 0; index < nums.length; index++){
+         if(nums[index] < 0)
+           set.add(nums[index]);
+       }
+
+       for(int index = 0 ; index < nums.length; index++){
+    
+         if(nums[index] > max &&  nums[index] > 0 && set.contains(-nums[index])){
+                ans = nums[index] ;
+                max = nums[index];
+         }
+       }
+        
+       return ans;
+       
+    }
+}
