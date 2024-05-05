3a4c83480255a120d28371ef9316725e00a4dd95
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-05-05 12:42:48 +0530

    Time: 6 ms (52.25%) | Memory: 68.1 MB (48.06%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -8,68 +8,65 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- /* Approach. 2 - Middle of LL + reverse 2n half & Two Pointer approach
- 1. Find the middle 
- 2. Reverse 2nd half of LL
- 2. Using two pointer check array is palindrom or not
 
- TC - o(n) + 0(n/2) = 0(n)
- SC = 0(1)
- */
 
+ ////1 2 3 1 2
+     //  2 1
 class Solution {
 
     public boolean isPalindrome(ListNode head) {
-      if(head == null || head.next == null)
+     if(head == null || head.next == null)
         return true;
 
-      ListNode middle = getMiddle(head);
-      ListNode right = reverseLL(middle.next);
-      ListNode left = head;
+     ListNode middle = middleNode(head);
 
-      while(right != null){
-          if(left.val != right.val)
-            return false;
+     ListNode left = head;
+     ListNode right = reverseList(middle);
 
-          left = left.next;
-          right = right.next;
-      }
-
-      return true;
-      
-    }
-
-    ListNode reverseLL(ListNode head){
-        if(head == null)
-            return head;
+     while(right != null){
+        if(left.val != right.val)
+           return false;
         
-        ListNode prev = null;
-        ListNode current = head;
+        left = left.next;
+        right = right.next;
+     }
+     
+      return true;
 
-        while(current != null){
-            ListNode next = current.next;
-
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        return prev;
     }
 
-    ListNode getMiddle(ListNode head){
+    ListNode middleNode(ListNode head){
         if(head == null || head.next == null)
-          return null;
+           return head;
 
         ListNode slowPointer = head;
         ListNode fastPointer = head;
 
-        while(fastPointer.next != null && 
-            fastPointer.next.next != null){
+        while(fastPointer  != null && fastPointer.next != null){
             slowPointer = slowPointer.next;
             fastPointer = fastPointer.next.next;
         }
 
         return slowPointer;
+    }
+
+    ListNode reverseList(ListNode head){
+        if(head == null || head.next == null)
+            return head;
+
+       ListNode current = head;
+       ListNode prev = null;
+
+       while(current != null){
+        ListNode next = current.next;
+
+        current.next = prev;
+        
+        prev = current;
+        current = next;
+
+       }
+
+       return prev;
     }
 }
