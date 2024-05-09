53a16774606e91e29858f856948059926b823ac2
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-05-09 20:22:01 +0530

    Time: 2 ms (79.18%) | Memory: 44.8 MB (49.01%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,52 @@
+/* 
+   0 1 2
+   1 1 2
+
+0 , 1, 2  takes
+   [1 1 2]
+
+1 takes
+[1]
+
+
+1  take
+   
+*/
+class Solution {
+     List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>(); 
+        Arrays.sort(nums); 
+	
+        solve(ans, new ArrayList<>(), nums, new boolean[nums.length]); 
+		
+        return ans;
+    }
+
+     void solve(List<List<Integer>> ans, List<Integer> temp, int[] nums, boolean[] vis){
+      if(nums.length == temp.size()) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+		
+        /*
+			Traverse for whole array one by one.
+		*/
+        for(int i=0;i<nums.length; i++) {
+		
+            if(vis[i]) continue; // if current element is already present in the temp, skip the element
+            if(i > 0 && ! vis[i-1] && nums[i] == nums[i-1]) continue; // if prev element and current element is equal, skip the element
+            
+            vis[i] = true;
+            temp.add(nums[i]);
+            
+            solve(ans, temp, nums, vis); // recursive call
+            
+			// backtracking
+            temp.remove(temp.size() - 1);
+            vis[i] = false;
+        }
+    }
+
+}
