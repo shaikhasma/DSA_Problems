3d2092009727341533a56412373af17669ebf3be
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-05-10 06:23:33 +0530

    Time: 9 ms (59.71%) | Memory: 54 MB (82.54%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,59 +1,72 @@
-/* approach - DFS
-1. traverse all boundary lands (1's')
-    - call dfs for that
-    - mark it as visited
-    - it should be land  + it should not visited + it should be valid to call next connected cell
+class Node{
+    int row;
+    int col;
+    public Node(int row, int col){
+      this.row = row;
+      this.col = col;
+    }
+}
 
-2. traverse complete land 
-  - check land which is not visited
-  - count it
-
-  TC - 0(N * M * 4) + 0(NM)  = 0(2 * NM) = 0(NM)
-  SC - 0(NM)  + 0(NM)
-        visited   stack auxillary
-
-  [0,0,0,0],
-  [1,0,1,0],
-  [0,1,1,0],
-  [0,0,0,0]
-    
-*/
 class Solution {
     public int numEnclaves(int[][] grid) {
         boolean[][] visited = new boolean[grid.length][grid[0].length];
-
-        for(int row = 0; row < grid.length; row++){
-            for(int col = 0; col < grid[0].length; col++){
-                if(row == 0 || row == grid.length - 1 || col == 0 || col == grid[0].length - 1){
-
-                    if(!visited[row][col] &&  grid[row][col] == 1){
-                        dfs(grid, row, col, visited);
-                    }
-                }
+        Queue<Node> q = new LinkedList<>();
+        // first row & last row
+        for(int index = 0; index < grid[0].length; index++){
+            if(grid[0][index] == 1 && !visited[0][index]){
+                q.add(new Node(0, index));
+                 visited[0][index] = true;
             }
-        }
-        
-        int enclaves = 0;
-        for(int row = 0; row < grid.length; row++){
-            for(int col = 0; col < grid[0].length; col++){
-                if( grid[row][col] == 1 && !visited[row][col]){
-                    enclaves++;
-                }
+            
+            if(grid[grid.length - 1][index] == 1 && !visited[grid.length - 1][index]){
+                q.add(new Node(grid.length - 1, index));
+                visited[grid.length - 1][index] = true;
             }
+            
         }
 
-        return enclaves;
-    }
+        // first col & last Col
+        for(int index = 0; index < grid.length; index++){
+            if(grid[index][0] == 1 && !visited[index][0]){
+                q.add(new Node(index, 0));
+                 visited[index][0] = true;
+            }
 
-    void dfs(int[][] grid, int row, int col, boolean[][] visited){
-        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || visited[row][col] || grid[row][col] == 0)
-            return;
+            if(grid[index][grid[0].length - 1] == 1 && !visited[index][grid[0].length - 1]){
+                q.add(new Node(index, grid[0].length - 1));
+                 visited[index][grid[0].length - 1] = true;
+            }
+        }
 
-        visited[row][col] = true;
-        
-        dfs(grid, row + 1, col, visited);
-        dfs(grid, row - 1, col, visited);
-        dfs(grid, row, col - 1, visited);
-        dfs(grid, row, col + 1, visited);
+        int[] dRow = {-1, 0, 1, 0};
+        int[] dCol = {0, 1, 0, -1}; //{0, +1, +0, -1}; 
+
+        while(!q.isEmpty()){
+            Node node = q.poll();
+         
+            for(int index = 0; index < 4; index++){
+                   //System.out.println(node.col + "---"+ index);
+                int nRow = node.row + dRow[index];
+                int nCol = node.col + dCol[index];
+
+                if(nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol <  grid[0].length &&
+                    grid[nRow][nCol] == 1 && !visited[nRow][nCol]){
+                    q.add(new Node(nRow,nCol));
+                     visited[nRow][nCol] = true;
+                }
+              
+            }
+        }
+
+        // Traverse Grid
+        int landCells = 0;
+        for(int row = 0; row < grid.length; row++){
+            for(int col = 0; col < grid[0].length; col++){
+                if(grid[row][col] == 1 && !visited[row][col])
+                     landCells++;
+            }
+        }
+
+        return landCells;
     }
 }
