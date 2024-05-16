afe9afab106d613ab8dc8d656a8142ad87528214
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-05-17 05:14:29 +0530

    Time: 81 ms (18.61%) | Memory: 44.5 MB (36.62%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,41 +9,51 @@
  * }
  */
 
- /* Approach - 3 Using Min Heap or Priority Queue
- TC - 0(n * k) * log k where k list total list size & n equal to  
-    - 0(n * k * logk)
- SC - 0(k)
+ /* Approach - 2  Merge  linked lists
+ TC - 
+    - 
+ SC - 
     
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        Queue<ListNode> q = new PriorityQueue<>((l1, l2)-> l1.val - l2.val);
-
-       ListNode dummyHead = new ListNode(-1);
-       ListNode dummyTemp = dummyHead;
-
+        if(lists == null || lists.length == 0) return null;
+      
+      
+        ListNode mergedListHead = null;
         for(int index = 0; index < lists.length; index++){
-
-            if(lists[index] != null)
-                q.add(lists[index]);
+         
+             mergedListHead = mergeSort(mergedListHead, lists[index]);
         }
 
-        if(q.isEmpty())
-            return null;
+        return mergedListHead;
         
+    }
+    ListNode mergeSort(ListNode l1, ListNode l2){
+      if(l1 == null) return l2;
+      if(l2 == null) return l1;
+      ListNode temp1 = l1;
+      ListNode temp2 = l2;
+      ListNode dummyHead = new ListNode(-1);
+      ListNode dummyTemp = dummyHead;
 
-        while(!q.isEmpty()){
-            ListNode popped = q.poll();
+      while(temp1 != null && temp2 != null){
+           if(temp1.val < temp2. val){
+               dummyTemp.next = temp1;
+               temp1 = temp1.next;
+           }else{
+               dummyTemp.next = temp2;
+               temp2 = temp2.next;
+           }
+           dummyTemp = dummyTemp.next;
+      }
 
-            if(popped.next != null) 
-                q.add(popped.next);
+      if(temp1 != null)
+         dummyTemp.next = temp1;
+      else
+         dummyTemp.next = temp2;
 
-            dummyTemp.next = popped;
-            dummyTemp = dummyTemp.next;
-        }
-
-        return dummyHead.next;
-           
-        }
+      return dummyHead.next;
+    }
         
 }
