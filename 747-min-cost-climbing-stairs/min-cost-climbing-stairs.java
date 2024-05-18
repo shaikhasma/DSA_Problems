8ab98923de47ad81da9bfc76483f799084146a79
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-05-18 22:54:03 +0530

    Time: 1 ms (39.46%) | Memory: 43.4 MB (13.43%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,34 @@
+/* Approach - Recursion + Memoization
+
+[10,15,20]
+      --- 2 [20]
+   ---  1 [15]
+--- 0 [10]
+
+  Nth stairs == N - 1  , N  - 2
+             min(cost.   cost)
+
+  0th == 0
+  1th  = 0
+  TC - 0(N)
+  SC - 0(N)   auxlillary space 0(2^N)
+
+*/
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+       Integer[] dp = new Integer[cost.length + 1];
+       return solve(cost, cost.length, dp);        
+    }
+
+    int solve(int[] cost, int n, Integer[] dp){
+        if(n == 0 || n == 1 ) return 0;
+        
+        if(dp[n] != null)
+            return dp[n];
+        
+        return dp[n] = Math.min (
+            solve(cost, n - 1, dp) + cost [n - 1], 
+            solve(cost, n - 2, dp) + cost[n - 2]
+        );
+    }
+}
