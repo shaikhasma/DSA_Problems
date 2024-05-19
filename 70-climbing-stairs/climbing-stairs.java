571d998134cba0aa8615b243a919eab76f81b375
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-05-19 08:55:22 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (84.91%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,17 +1,19 @@
-/*. Approach - 3 Tabulation 
+/*. Approach - 4 Space Optmization
 TC - 0(N)
-SC - 0(N)   
+SC - 0(1)   
      dp. 
 */
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
+        int prev1 = 1;
+        int prev2 = 1;
 
-        for(int index = 2; index <= n; index++)
-            dp[index] = dp[index - 1] + dp[index - 2];
-        
-        return dp[n];
+        for(int index = 2; index <= n; index++){
+          int current = prev1 + prev2;
+          prev2 = prev1;
+          prev1 = current;
+        }        
+     
+       return prev1;
     }
 }
