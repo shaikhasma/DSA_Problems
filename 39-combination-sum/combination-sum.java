146b42824b17402605235c33dfdda1f649cd4f4b
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-05-26 11:00:33 +0530

    Time: 2 ms (85.34%) | Memory: 44.6 MB (43.28%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,4 +1,31 @@
+/*
+   Approach - 1 Recursion + Backtraking
+   Start with 0 index (index )
 
+   1 Traverse i from index to last index
+
+   2. Pick the element when element is less than target
+    -- target - picked element or reduce target by picked element
+
+   3. call function recursively with the same element to check possiblity of pick again or not
+
+   4. here if we come back from recurive call 
+    -- remove current element as next step we are going to exclude or not pick current element 
+
+  Repeate all 2 3 4 steps till i <= last index 
+
+
+   Base Case 
+   1. - While performing any option if we find target before reaching to last index 
+      - fill temp list of comibination into ans & return back 
+
+      TC - 0(2^t) * k 
+          -  where t is target as we can pick same element multiple time we can pick 1 10 time if target is 10
+          - k is avg length of every generated compination which we fill to ans list which will take linear time not constant
+
+      SC - We can not predict space as it completly depedent on number combination
+        - 0(K * C) here k is avg lenght of combination & c is number of combination
+*/
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> list = new ArrayList<>();
