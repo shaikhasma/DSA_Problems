29db4eac1dde7c00e5dcfe9a266d309a0db24b1b
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-03 05:22:06 +0530

    Time: 7 ms (51.76%) | Memory: 45 MB (67.57%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -72,7 +72,7 @@ Program -
         return dp[row][col] = Math.min(up, Math.min(leftDiagonal, rightDiagonal));
     }
 
-Approach - 2 Tabulation / Bottom Up
+Approach - 3 Tabulation / Bottom Up
 TC - 0(NM)
 SC - 0(NM)
     stack.   dp
@@ -117,6 +117,60 @@ Program -
         }
         return minSum;    
     }
+Approach - 4 Space Optimization 
+    - User prev Row fill with 0th row of  grid 
+    - traverse from 1st row of grid 
+      - take prev row left Diagonal , direct up, right diagonal cell add into current cell
+      - find the min betweeen above three & set to currentRow[col]
+
+      - Set prevRow to currentRow
+    
+    - Find max from n-1 last row i.e prevRow return max value from this
+TC - 0(NM)
+SC - 0(N) + 0(N)
+
+Program - 
+    public int minFallingPathSum(int[][] matrix) {
+        int totRow = matrix.length;
+        int totCol = matrix[0].length;
+        int minSum = Integer.MAX_VALUE;
+
+        int[] prevRow = new int[totCol];
+        for( int col = 0; col < totCol ; col++ )
+            prevRow[col] = matrix[0][col];
+        
+        for(int row = 1; row < totRow; row++){ 
+            
+            int[] currentRow = new int[totCol];
+            
+            for(int col = 0; col < totCol; col++){
+            
+                int up = matrix[row][col] + prevRow[col];
+                int leftDiagonal = matrix[row][col];
+                int rightDiagonal = matrix[row][col];
+
+                if( col > 0)
+                    leftDiagonal += prevRow[col - 1];
+                else 
+                    leftDiagonal += (int) 1e9;
+
+                if( col < matrix[0].length - 1)
+                    rightDiagonal += prevRow[col + 1];
+                 else 
+                    rightDiagonal += (int) 1e9;
+
+                currentRow[col] = Math.min(up, Math.min(leftDiagonal, rightDiagonal));
+            }
+            prevRow = currentRow;
+        }
+
+        for(int col = 0; col < totCol; col++){
+            minSum = Math.min(minSum, prevRow[col]);
+        }
+        return minSum;    
+    }
+
+
 
 */
 
@@ -126,37 +180,37 @@ class Solution {
         int totCol = matrix[0].length;
         int minSum = Integer.MAX_VALUE;
 
-        int[][] dp = new int[totRow][totCol];
-        for( int[] dpRow : dp)
-            Arrays.fill(dpRow, -1);
-        
-        //keep 0th row of matrix into DP[0][col]
-        for(int col = 0 ; col < totCol; col++)
-            dp[0][col] = matrix[0][col];
+        int[] prevRow = new int[totCol];
+        for( int col = 0; col < totCol ; col++ )
+            prevRow[col] = matrix[0][col];
         
         for(int row = 1; row < totRow; row++){ 
+            
+            int[] currentRow = new int[totCol];
+            
             for(int col = 0; col < totCol; col++){
             
-                int up = matrix[row][col] + dp[row - 1][col];
+                int up = matrix[row][col] + prevRow[col];
                 int leftDiagonal = matrix[row][col];
                 int rightDiagonal = matrix[row][col];
 
                 if( col > 0)
-                    leftDiagonal += dp[row - 1][col - 1];
+                    leftDiagonal += prevRow[col - 1];
                 else 
                     leftDiagonal += (int) 1e9;
 
                 if( col < matrix[0].length - 1)
-                    rightDiagonal += dp[row - 1][col + 1];
+                    rightDiagonal += prevRow[col + 1];
                  else 
                     rightDiagonal += (int) 1e9;
 
-                dp[row][col] = Math.min(up, Math.min(leftDiagonal, rightDiagonal));
+                currentRow[col] = Math.min(up, Math.min(leftDiagonal, rightDiagonal));
             }
+            prevRow = currentRow;
         }
 
         for(int col = 0; col < totCol; col++){
-            minSum = Math.min(minSum, dp[totRow - 1][col]);
+            minSum = Math.min(minSum, prevRow[col]);
         }
         return minSum;    
     }
