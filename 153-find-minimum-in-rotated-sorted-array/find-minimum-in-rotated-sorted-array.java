30bd151f76adbe6de207d426ee427a48ad795c5c
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-09 01:01:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (9.97%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,46 +1,29 @@
 /*
-Appraoch = Binary Search
- 0 1 2 3 4 
-[3,4,5,1,2]
-
-TC- 0(lonN)
-SC - 0(1)
-
-
-take min = +Infinity
-
-compare min with mid if less keep min value in min
-
-if left / righ high is sorted or not
-take min value compare with min variable and keep min in min variable from sorted half and go ahead....
-
-
-
+[4,5,6,7,0,1,2]
+ 0 1 2 3 4 5 6
+         l.  g 
+ min +
+ which part sorted
+ before moving to other part take min from sorted part
 */
 class Solution {
-    public int findMin(int[] nums) {
-        int low = 0;
-        int high = nums.length - 1;
+    public int findMin(int[] arr) {
         int min = Integer.MAX_VALUE;
 
+        int low = 0;
+        int high = arr.length - 1;
 
         while(low <= high){
-
-            int mid = (high + low) / 2;
+            int mid = high - ( high - low) / 2;
             
-            min = Math.min(min, nums[mid]);
-
-            if(nums[low] <= nums[mid]){
-                min = Math.min(min, nums[low]);
+            if(arr[low] <= arr[mid]){
+                min = Math.min(min, arr[low]);
                 low = mid + 1;
-            }
-            else{
-                min = Math.min(min, nums[mid]);
+            }else{
+                min = Math.min(min, arr[mid]);
                 high = mid - 1;
             }
-
         }
-
         return min;
     }
 }
