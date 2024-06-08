1010770ca68100cca1ed9057799ad4052b73d1eb
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-08 23:12:16 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (93.36%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,66 +1,58 @@
-/*
-[5,7,7,8,8,10]
- 0 1 2 3 4 5
-       
-*/
-
 class Solution {
-    public int[] searchRange(int[] A, int target) {
-        int[] ans = new int[2];
-        ans[0] = firstPosition(A, target);
-        ans[1] = lastPosition(A, target);
-        return ans;
-
+    public int[] searchRange(int[] arr, int target) {
+        // 5,7,7,8,8,10 target 8 
         
+
+        int firstIndex = lowerBound(arr, target);
+        int lastIndex = -1;
+
+        if(firstIndex != -1) {
+          lastIndex = upperBound(arr, target) ;
+          lastIndex = lastIndex == -1 ? arr.length - 1: lastIndex - 1; 
+        } 
+
+        return new int[] {firstIndex , lastIndex};
     }
-
-    int firstPosition(int[] A, int target){
+    
+    int lowerBound(int[] arr, int target){
         int low = 0;
-        int high = A.length - 1;
-       
-        
-        int firstPosition = -1;
-        while(low <= high){
-            int mid = high - ( high - low) / 2;
-            
-            if(A[mid] == target){
-                firstPosition = mid;
-                high = mid - 1;
-            }
-            else if( A[mid] < target){
-               low = mid + 1;
-               
-            }
-            else{
-                high = mid - 1;
-            }
-        }
+        int high = arr.length - 1;
+         int lowerBound = -1;
 
-        return firstPosition;
-    }
-
-    int lastPosition(int[] A, int target){
-        int low = 0;
-        int high = A.length - 1;
-       
-        
-        int lastPosition = -1;
         while(low <= high){
-            int mid = high - ( high - low) / 2;
-            
-            if(A[mid] == target){
-                lastPosition = mid;
+            int mid = high - (high - low) / 2 ;
+
+            if(target == arr[mid])
+                lowerBound = mid;
+
+            if(target <= arr[mid]){
+                high = mid - 1;
+            }else{
                 low = mid + 1;
             }
-            else if( A[mid] < target){
-               low = mid + 1;
-            }
-            else{
-                high = mid - 1;
-            }
         }
 
-        return lastPosition;
-    }
+        return lowerBound;
+    }   
+
+     int upperBound(int[] arr, int  target){
+        int low = 0;
+        int high = arr.length - 1;
+        int upperBound = -1;
+        
+        while( low <= high){
+            int mid = high - (high - low) / 2;
+
+            if(target < arr[mid]){
+                upperBound = mid;
+                high = mid - 1;
+            }
+            else
+               low = mid + 1;
+        }
+
+        return upperBound;
+     }
+
 
 }
