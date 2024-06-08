fa6d478d6ee286861c7dfe6ecd64ee9ffd19efe3
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-09 00:38:49 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (40.66%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,48 +1,57 @@
-/* Approach - 2 Binary Search
-TC = 0(logN)
-SC = 0(1)
+/*
+ [2,5,6,0,0,1,2]. 3
+  0 1 2 3 4 5  6
 
-
-0 1 2 3 4 5 6
-3 3 4 3 3 3 3
-    l m h
-
+ [2,2 2,2 3,0,1,2] 1
+  0 1 3 4 5 6 7  8
+  
+  low = mid = high. not apply
+  low ++ 
+  high --
 
 */
+
 class Solution {
-    public boolean search(int[] nums, int target) {
-       int low = 0;
-       int high = nums.length - 1;
-
-       while(low <= high){
-        int mid = (high + low ) /2;
+    public boolean search(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
         
-        if( nums[mid] == target) 
-            return true;
+        if(arr.length == 1 && target != arr[0])
+            return false;
+            
+        while(low <= high){
+            int mid = high - (high - low) / 2;
+            // [1,0,1,1,1]
+            //  0 1 2 3 4
+             //     l.m  h
+            if(arr[mid] == target)
+                return true;
 
-        if( nums[low] == nums[mid] && nums[mid] == nums[high]){
-            low++;
-            high--;
-            continue;
-        }
-         // left sorted
-        if(nums[low] <= nums[mid]){
-            if(nums[low]<= target && nums[mid] >= target){
-                high = mid - 1;
+            if(arr[low] == arr[mid] && arr[mid] == arr[high]){
+                //shrink 
+                low++;
+                high--;
+                continue;
+            }
+
+            if(arr[mid] >= arr[low]){
+                if(target >= arr[low] && target <= arr[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+
             }else{
-                low = mid + 1;
-            }
-        }else{
-            if(nums[mid]<= target && nums[high] >= target){
-                low = low + 1;
-            }
-            else{
-                high = high - 1;
+                if(target >= arr[mid] && target <= arr[high]){
+                    low = mid + 1;
+                }else{
+                   high = mid - 1;
+                }
             }
         }
 
-       }
+        return false;
 
-       return false;
     }
 }
