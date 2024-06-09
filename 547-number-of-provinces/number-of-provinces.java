68dd1eed2457f43f26587e25414f6ac2ee522a15
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-09 19:11:34 +0530

    Time: 3 ms (37.36%) | Memory: 46.7 MB (85.26%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,57 +1,56 @@
 /*
-  Approach - DFS
+      0 1 2
+    0 1,1,0
+    1 1,1,0
+    2 0,0,1
 
-  count how many time we have started new DFS
-  if diconnected graph then we have to start graph those many number of time which is equal to total components
-
-  TC - 0(V+E)
-  SC - 0(V)
-  
-  */
+    0 -- 1
+    1 ---0
+    2. ---
+     0 1 2
+    [T T T]
+*/
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-       
-       ArrayList<ArrayList<Integer>> adjList = prepareAdjList(isConnected);
-       boolean[] visited = new boolean[isConnected.length];
-       int count = 0;
+    public int findCircleNum(int[][] graph) {
+        int component = 0;
+        ArrayList<ArrayList<Integer>> adjList = prepareAdjList(graph);
+        boolean[] visited = new boolean[graph.length];
 
-       for(int index = 0; index < isConnected.length; index++){
-         if(!visited[index]){
-            count++;
-            dfs(index, adjList, visited );
-         }
-       }
-
-       return count;
-    }
-
-    void dfs(int node, ArrayList<ArrayList<Integer>> adjList, boolean[] visited){
-       visited[node] = true;
-
-       for(Integer neighbor : adjList.get(node)){
-         if(!visited[neighbor]){
-            dfs(neighbor, adjList, visited);
-         }
-       }
-    }
-
-    ArrayList<ArrayList<Integer>> prepareAdjList(int[][] edges){
-
-      ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
-
-        for(int index = 0; index < edges.length; index++){
-            adjList.add( new ArrayList<Integer>());
+        for( int node = 0 ; node < graph.length; node++){
+          if(!visited[node]){
+            component++;
+            dfs(node, adjList, visited);
+          }
         }
 
-        for(int row = 0; row < edges.length; row++){
-            for(int col = 0; col < edges.length; col++){
-            
-                if(edges[row][col] == 1)
-                adjList.get(row).add(col);
-            
+        return component;
+    }
+  
+    void dfs(int node, ArrayList<ArrayList<Integer>> adjList, boolean[] visited){
+        visited[node] = true;
+
+        for( Integer neighbor : adjList.get(node)){
+            if(!visited[neighbor]){
+                dfs(neighbor, adjList, visited);
+            }
+        }
+    }
+
+    ArrayList<ArrayList<Integer>> prepareAdjList(int[][] graph){
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        for(int index = 0; index < graph.length; index++){
+            list.add(new ArrayList<>());
+        }
+       
+        for(int row = 0; row < graph.length; row++){
+            for(int col = 0; col < graph[0].length; col++){
+                if(graph[row][col] == 1){
+                    list.get(row).add(col);
+                }
             }
         }
 
-       return adjList; 
+        return list;
     }
 }
