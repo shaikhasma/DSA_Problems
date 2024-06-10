8265162de8ba79a9459ea1ae75ed2d6d20390369
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-10 11:57:53 +0530

    Time: 3 ms (93.90%) | Memory: 44.5 MB (66.24%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,46 +1,48 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prereq) {
-      boolean[] visited = new boolean[numCourses];
-      boolean[] dfsVisited = new boolean[numCourses];
-      ArrayList<ArrayList<Integer>> adjList = prepareAdjList(numCourses, prereq);
+        boolean[] visited = new boolean[numCourses];
+        boolean[] dfsVisited = new boolean[numCourses];
+        ArrayList<ArrayList<Integer>> adjList = prepareAdjList(numCourses, prereq);
 
-      for(int course = 0; course < numCourses; course++){
-         if(!visited[course]){
-             if(dfs(course, adjList, visited, dfsVisited)){
-                 return false;
-             }
-         }
-      }
+        for(int course = 0; course < numCourses; course++){
+            if(!visited[course]){
+                if(dfs(course, adjList, visited, dfsVisited)){
+                    return false;
+                }
+            }
+        }
       return true;
     }
 
     boolean dfs(int node, ArrayList<ArrayList<Integer>> adjList, boolean[] visited,
                     boolean[] dfsVisited){
-    visited[node] = true;
-    dfsVisited[node] = true;
+        visited[node] = true;
+        dfsVisited[node] = true;
 
-    for( Integer neighbor : adjList.get(node)){
-        if(!visited[neighbor]){
-            if(dfs(neighbor, adjList, visited, dfsVisited)){
+        for( Integer neighbor : adjList.get(node)){
+            if(!visited[neighbor]){
+                if(dfs(neighbor, adjList, visited, dfsVisited)){
+                    return true;
+                }
+            }else if(dfsVisited[neighbor]){
                 return true;
             }
-        }else if(dfsVisited[neighbor]){
-            return true;
         }
+
+        dfsVisited[node] = false;
+
+        return false;
     }
 
-    dfsVisited[node] = false;
-    return false;
-    }
     private ArrayList<ArrayList<Integer>> prepareAdjList(int numCourses, int[][] prereq){
         ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
         for(int course = 0 ; course < numCourses ; course++){
             adjList.add(new ArrayList<>());
         }
       
-        for(int i = 0 ; i < prereq.length; i++){
+        for(int i = 0 ; i < prereq.length; i++)
             adjList.get(prereq[i][1]).add(prereq[i][0]);
-        }
+        
        return adjList;
     }
 }
