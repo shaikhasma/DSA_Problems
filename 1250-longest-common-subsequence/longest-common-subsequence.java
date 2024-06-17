4ff41af2ab6cbe6f6e082563c40594aea31c062e
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-17 23:46:56 +0530

    Time: 41 ms (6.04%) | Memory: 51 MB (17.98%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,6 +1,53 @@
-/* Approach - 2. Recursion + Memorization
+/* 
+Approach - 1 Recursion
+TC - 0(2^N * 2^M)
+SC - -0(N + M)
+
+Program - 
+     public int longestCommonSubsequence(String text1, String text2) {
+        return solv(text1, text1.length() - 1, text2, text2.length() - 1);
+    }
+
+    int solv(String str1, int index1, String str2, int index2){
+        if(index1 < 0 || index2 < 0)
+            return 0;
+
+        if(str1.charAt(index1) == str2.charAt(index2)){
+            return 1 + solv(str1, index1 - 1, str2 , index2 - 1);
+        }
+
+        return Math.max(solv(str1, index1 - 1, str2, index2), solv(str1, index1, str2, index2 - 1));
+
+    }
+
+Approach - 2. Recursion + Memorization
 TC - 0(NM)
 SC - -0(NM) + 0(N + M))
+Program - 
+        public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length()][text2.length()];
+
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+        
+        return solv(text1, text1.length() - 1, text2, text2.length() - 1,dp);
+    }
+
+    int solv(String str1, int index1, String str2, int index2, int[][] dp){
+        if(index1 < 0 || index2 < 0)
+            return 0;
+
+        if(dp[index1][index2] != -1)
+            return dp[index1][index2];
+
+        if(str1.charAt(index1) == str2.charAt(index2)){
+            return dp[index1][index2] = 1 + solv(str1, index1 - 1, str2 , index2 - 1, dp);
+        }
+
+        return dp[index1][index2] = Math.max(solv(str1, index1 - 1, str2, index2, dp), solv(str1, index1, str2, index2 - 1, dp));
+
+    }
+
 */
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
