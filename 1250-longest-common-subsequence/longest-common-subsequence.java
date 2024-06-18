84a75fbdccabab4f4a9342b3dae2709701609f07
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-18 07:34:33 +0530

    Time: 31 ms (14.20%) | Memory: 50.2 MB (87.20%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -48,29 +48,57 @@ Program -
 
     }
 
-*/
-class Solution {
+Approach - 3. Tabulation
+TC - 0(NM)
+SC - -0(NM)
+Program - 
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length()][text2.length()];
-
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
         for(int[] row : dp)
             Arrays.fill(row, -1);
         
-        return solv(text1, text1.length() - 1, text2, text2.length() - 1,dp);
+        // set 1st row & 1st col with 0
+        for(int index = 0; index <= text1.length(); index++)
+            dp[index][0] = 0;
+        for(int index = 0; index <= text2.length(); index++)
+            dp[0][index] = 0;
+
+        for(int index1 = 1; index1 <= text1.length(); index1++){
+            for( int index2 = 1; index2 <= text2.length(); index2++){
+
+                if(text1.charAt(index1 - 1) == text2.charAt(index2 - 1))
+                    dp[index1][index2] = 1 + dp[index1 - 1][index2 - 1];
+                else
+                    dp[index1][index2] = Math.max(dp[index1 - 1][index2] , dp[index1][index2 - 1]);
+            }
+        }
+        
+        return dp[text1.length()][text2.length()];
     }
 
-    int solv(String str1, int index1, String str2, int index2, int[][] dp){
-        if(index1 < 0 || index2 < 0)
-            return 0;
+*/
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+        
+        // set 1st row & 1st col with 0
+        for(int index = 0; index <= text1.length(); index++)
+            dp[index][0] = 0;
+        for(int index = 0; index <= text2.length(); index++)
+            dp[0][index] = 0;
 
-        if(dp[index1][index2] != -1)
-            return dp[index1][index2];
+        for(int index1 = 1; index1 <= text1.length(); index1++){
+            for( int index2 = 1; index2 <= text2.length(); index2++){
 
-        if(str1.charAt(index1) == str2.charAt(index2)){
-            return dp[index1][index2] = 1 + solv(str1, index1 - 1, str2 , index2 - 1, dp);
+                if(text1.charAt(index1 - 1) == text2.charAt(index2 - 1))
+                    dp[index1][index2] = 1 + dp[index1 - 1][index2 - 1];
+                else
+                    dp[index1][index2] = Math.max(dp[index1 - 1][index2] , dp[index1][index2 - 1]);
+            }
         }
-
-        return dp[index1][index2] = Math.max(solv(str1, index1 - 1, str2, index2, dp), solv(str1, index1, str2, index2 - 1, dp));
-
+        
+        return dp[text1.length()][text2.length()];
     }
 }
