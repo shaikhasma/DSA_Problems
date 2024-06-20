35653ac273ea97037b9bf727faf7166cb4de0b79
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-20 20:52:57 +0530

    Time: 2 ms (15.38%) | Memory: 45.4 MB (74.14%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -38,39 +38,37 @@ f(index, isBuy)
 -3 + 6 = 3
 
 4
- 0 to n - 1
+ n - 1 to 0
  true false
  int[][] dp = new int[n][2] -1
+-1 -1
+-1 -1
 
 */
 class Solution {
-    public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][2];
+    public int maxProfit(int[] arr) {
+        int[][] dp = new int[arr.length + 1][2];
         for( int[] row : dp)
             Arrays.fill(row, -1);
+        
+        dp[arr.length][0] = 0;
+        dp[arr.length][1] = 0;
 
-        return solv(prices, 0, 0, dp);
-    }
+        int profit = 0;
+        for(int index = arr.length - 1; index >= 0; index-- ){
+            for(int buy = 0; buy <= 1; buy++){
+                if(buy == 0){
+                    profit = Math.max(-arr[index]  + dp[index + 1][1],
+                            dp[index + 1][0]);
+                }
+                else{
+                    profit = Math.max(arr[index] + dp[index + 1][0],
+                                 dp[index + 1][1]);
+                }
 
-    int solv(int[] arr, int index, int isBuy, int[][] dp){
-    
-     if(index == arr.length)
-        return 0;
-
-     if(dp[index][isBuy] != -1)
-        return dp[index][isBuy];
-
-      int pick = 0; 
-      int notPick = 0;
-      if(isBuy == 0){
-        pick = -arr[index]  + solv(arr, index + 1, 1, dp);
-        notPick = solv(arr, index + 1, 0, dp);
-      }
-      else{
-        pick = arr[index] + solv(arr, index + 1, 0,dp);
-        notPick = solv(arr, index + 1, 1, dp);
-      }
-      
-      return dp[index][isBuy] = Math.max(pick, notPick);
+                dp[index][buy] = profit;
+            }
+        }
+        return dp[0][0];
     }
 }
