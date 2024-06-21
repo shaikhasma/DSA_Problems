78c064835a67dfad35e4d36108a1a201fc83305c
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-21 22:35:47 +0530

    Time: 177 ms (6.63%) | Memory: 93.2 MB (5.20%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,24 +1,71 @@
+/*
+  0  1 2 3 4 
+  [1,2,3,4,5]
+
+  profit = 2 - 1  = 1
+           2 
+
+  Max = sell - buy
+  buy or not buy based on  isBuy = false 
+  sell or not sell based on isBuy = true
+  
+  0 to  n - 1
+  f(arr, 0 , false, 2)
+  f(arr, index , isBuy, txt)
+  
+   if tx == 0 || indexx == n
+     return 0
+
+  
+  if buy == false
+     buy  -arr[index] + move next + true, tx
+     not buy move next + false tx
+  
+  if buy == true
+     sell arr[index] +move next + false tx  - 1
+     not sell move next + true; tx
+
+     max profit calcualte
+  4
+
+  TC - 0(2^N)
+  SC - 0(N)
+*/
 class Solution {
-    public int maxProfit(int[] prices) {
-    int sell1 = 0, sell2 = 0, buy1 = Integer.MIN_VALUE, buy2 = Integer.MIN_VALUE;
-		for (int i = 0; i < prices.length; i++) {
-			buy1 = Math.max(buy1, -prices[i]);
-			sell1 = Math.max(sell1, buy1 + prices[i]);
-			buy2 = Math.max(buy2, sell1 - prices[i]);
-			sell2 = Math.max(sell2, buy2 + prices[i]);
-		}
-		return sell2;   
+    public int maxProfit(int[] arr) {
+        int[][][] dp = new int[arr.length][2 + 1][2 + 1];
+        
+        for(int[][] temp : dp){
+            for(int[] row : temp){
+                Arrays.fill(row, -1);
+            }
+        }
+
+        return solv(arr, 0, 0, 2, dp);
+    }
+
+
+
+    int solv(int[] arr, int index, int canBuy, int tx, int[][][] dp){
+       if(tx == 0 || index == arr.length)
+            return 0;
+
+       int take = 0;
+       int notTake = 0;
+
+       if(dp[index][canBuy][tx] != -1 )
+            return dp[index][canBuy][tx];
+
+        if(canBuy == 0){
+            take = -arr[index] + solv(arr, index + 1, 1, tx, dp);
+            notTake = solv(arr, index + 1, 0, tx,dp); 
+        }
+        else{
+            take = arr[index] + solv(arr, index + 1, 0, tx - 1, dp);
+            notTake = solv( arr, index + 1, 1, tx, dp);
+        }
+
+        return dp[index][canBuy][tx] = Math.max(take, notTake);
+
     }
 }
-
-/*
-
-  10 22  5 75 65 80
-  10 12 -5 65 60 70
-  70 58 75  5 15 80
-
-  12 + 
-  75
-  ----
-  87
-*/
