9d438a24902b804355bba188b3f006e31461f066
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-22 12:11:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (11.12%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,94 +1,23 @@
-/* Approach - 1 Recursion
-  0 1 2 3
- [1 2 3 1]
-
-    f(3) ( max ( 3,4))
-        pick = 1 + f(1) // 2 = 3
-            pick = 2 + f(-1)// 0
-                -1 -- > 0
-        notPick = f(2) ==> 4 
-            pick = 3 + f(0)//1 == 4
-                1 -- > 1
-            notPick = f(1); // 2
-                pick = 2 + f(-1)//0
-                    -1 --> 0
-            max(4, 2)
-        
-TC - 0(2^N)
-SC - 0(N) stack space
-
-public int rob(int[] nums) {
-        return solve(nums, nums.length - 1);
-    }
-
-    int solve(int[] arr, int index){
-     if(index == 0)
-        return arr[index];
-     
-     if( index < 0) 
-        return 0;
-
-     int pick = arr[index] + solve(arr, index - 2 );
-     int notPick = solve(arr, index - 1);
-
-     return Math.max(pick, notPick);
-    }
-
-Approach - 2 Recursion + Memoiation
-    use dp array and store ans first time 
-TC - 0(N)
-SC - 0(N) + 0(N) 
-     dp[].   stack
-   - O(N) 
-
-   public int rob(int[] nums) {
-        Integer[] dp = new Integer[nums.length];
-        
-        return solve(nums, nums.length - 1, dp);
-    }
-
-    int solve(int[] arr, int index, Integer[] dp){
-     if(index == 0)
-        return arr[index];
-     
-     if( index < 0) 
-        return 0;
-
-     if(dp[index] != null)
-        return dp[index];
-
-     int pick = arr[index] + solve(arr, index - 2 , dp);
-     int notPick = solve(arr, index - 1, dp);
-
-     return dp[index] = Math.max(pick, notPick);
-    }
-
-
-Approach 3 - Tabulation // Bottom up approach
-
-    - use same dp array 
-    - Traverse 0 to N - 1
-    - rather than f(index - 2) or f(index -1) use dp[index - 2] and dp[index - 1]
-
-TC - 0(N)
-SC - 0(N)
-*/
 class Solution {
-    public int rob(int[] nums) {
-        Integer[] dp = new Integer[nums.length];
-        dp[0] = nums[0];
+    public int rob(int[] arr) {
+        int n = arr.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
 
-        for( int index = 1; index < nums.length; index++){
-            int pick = nums[index];
-            
-            if(index > 1)
-                pick +=  dp[index - 2];
-            
-            int notPick = dp[index - 1];
+        return solv(arr, n - 1, dp);
+    }
+    int solv(int[] arr, int index, int[] dp){
+        if(index < 0)
+            return 0;
 
-            dp[index] =  Math.max(pick, notPick);
-        }
+        if(index == 0)
+            return arr[0];
+        if(dp[index] != - 1)
+            return dp[index];
 
-        return dp[nums.length - 1];    
+        int pick = arr[index] + solv(arr, index - 2, dp);
+        int notPick = solv(arr, index - 1, dp);
+
+        return dp[index ] = Math.max(pick, notPick); 
     }
 }
