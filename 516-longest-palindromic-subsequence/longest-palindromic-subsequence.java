c0f8cdf853f6824f452c01d1d184932dc962fa85
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-22 22:51:16 +0530

    Time: 46 ms (53.44%) | Memory: 55.7 MB (7.27%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,5 +1,50 @@
 /*
-   Approach - 1 Tabulation 
+Approach - 1 Recursion
+Prorgram - 
+    public int longestPalindromeSubseq(String s) {
+        String strRev = new StringBuilder(s).reverse().toString();
+        return lcs(s, s.length() - 1, strRev, strRev.length() -1);
+    }
+    int lcs(String s1, int index1 , String s2, int index2){
+        if(index1 < 0 || index2 < 0)
+            return 0;
+
+        if(s1.charAt(index1) == s2.charAt(index2))
+            return 1 + lcs(s1, index1 - 1, s2, index2 - 1);
+        
+        int step1 = lcs(s1, index1 - 1, s2, index2);
+        int step2 = lcs(s1, index1, s2, index2 - 1);
+
+        return Math.max(step1, step2);
+    }
+Approach 2 - Memorization
+Program - 
+    public int longestPalindromeSubseq(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+
+        String strRev = new StringBuilder(s).reverse().toString();
+        return lcs(s, s.length() - 1, strRev, strRev.length() -1, dp);
+    }
+
+    int lcs(String s1, int index1 , String s2, int index2, int[][] dp){
+        if(index1 < 0 || index2 < 0)
+            return 0;
+
+        if(dp[index1][index2] != -1)
+            return dp[index1][index2];
+
+        if(s1.charAt(index1) == s2.charAt(index2))
+            return 1 + lcs(s1, index1 - 1, s2, index2 - 1, dp);
+        
+        int step1 = lcs(s1, index1 - 1, s2, index2, dp);
+        int step2 = lcs(s1, index1, s2, index2 - 1, dp);
+
+        return dp[index1][index2] = Math.max(step1, step2);
+    }
+
+   Approach - 3 Tabulation 
      0 1 2 3 4
 s1 = b b b a b
 s2 = b a b b b
@@ -91,31 +136,27 @@ s2 = b a b b b
 
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        String s1 = s;
-        String s2 = new StringBuilder(s).reverse().toString();
-        return lcs(s1,s2);
+        int[][] dp = new int[s.length()][s.length()];
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+
+        String strRev = new StringBuilder(s).reverse().toString();
+        return lcs(s, s.length() - 1, strRev, strRev.length() -1, dp);
     }
 
-    int lcs(String s1, String s2){
-        int n = s1.length();
-        int m = s2.length();
+    int lcs(String s1, int index1 , String s2, int index2, int[][] dp){
+        if(index1 < 0 || index2 < 0)
+            return 0;
 
-        int[] prevRow = new int[m + 1];
+        if(dp[index1][index2] != -1)
+            return dp[index1][index2];
 
-        for(int index1 = 1; index1 <= n; index1++){
-            int[] currentRow = new int[m +1];
-            for( int index2 = 1; index2 <= m; index2++){
-                //if both prev chars matches then 1 + take previous values 
-                if(s1.charAt(index1 - 1) == s2.charAt(index2 - 1)){
-                    currentRow[index2] = 1 + prevRow[index2 - 1];
-                }else{
-                    // take max  previous row & prevCol
-                    currentRow[index2] = Math.max(prevRow[index2], currentRow[index2 - 1]);
-                }
-            }
-            prevRow = currentRow;
-        }
+        if(s1.charAt(index1) == s2.charAt(index2))
+            return 1 + lcs(s1, index1 - 1, s2, index2 - 1, dp);
+        
+        int step1 = lcs(s1, index1 - 1, s2, index2, dp);
+        int step2 = lcs(s1, index1, s2, index2 - 1, dp);
 
-        return prevRow[m];
+        return dp[index1][index2] = Math.max(step1, step2);
     }
 }
