f27f4cfe1faa0ac6012d20d60e19b73e234f8530
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-22 18:28:23 +0530

    Time: 10 ms (51.96%) | Memory: 61 MB (53.73%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -99,9 +99,8 @@ Program -
 Approach - Tabulation Bottom Up approach
 TC - 0(NM)
 TC - 0(N*2*2) 
-    
-*/
-class Solution {
+
+Program - 
     public int maxProfit(int[] arr) {
         int[][][] dp = new int[arr.length + 1][2 + 1][2 + 1];
         
@@ -123,5 +122,39 @@ class Solution {
             }
         }
         return dp[0][0][2];
+    }
+
+
+    
+*/
+class Solution {
+    public int maxProfit(int[] arr) {
+        int[][] next = new int[2][3];
+        int[][] current = new int[2][3];
+        
+        for(int index = arr.length - 1; index >= 0 ; index-- ){
+            for(int buy = 0 ; buy <= 1; buy++){
+                for(int  tx = 1; tx <= 2; tx++){
+                    int take = 0;
+                    int notTake = 0;
+                    if(buy == 0){
+                        take = -arr[index] + next[1][tx];
+                        notTake = next[0][tx]; 
+                    }
+                    else{
+                        take = arr[index] + next[0][tx - 1];
+                        notTake = next[1][tx];
+                    }
+                    current[buy][tx] = Math.max(take, notTake);
+                }
+            }
+
+            for(int buy = 0 ; buy <= 1; buy++){
+                for(int tx = 1; tx < 3; tx++){
+                    next[buy][tx] = current[buy][tx];
+                }
+            }
+        }
+        return next[0][2];
     }
 }
