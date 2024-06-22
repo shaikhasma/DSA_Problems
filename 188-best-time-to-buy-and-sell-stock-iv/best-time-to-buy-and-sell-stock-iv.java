a021b2bbd70f8e3ea28dfd6b4426da18dbaff116
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-22 18:47:33 +0530

    Time: 9 ms (28.19%) | Memory: 44.4 MB (44.50%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,43 +1,64 @@
+/*
+    ============================================My Note ============================================================
+Approach - 1 Recursion
+TC - 0(2^N)
+SC - 0(N)
+
+Program - 
+    public int maxProfit(int k, int[] arr) {
+        return solv(arr, 0, 0, k);
+    }
+
+    int solv(int[] arr, int index, int canBuy, int tx){
+        
+        if(tx == 0 || index == arr.length)
+          return 0; 
+
+        int take = 0;
+        int notTake= 0;
+
+        if(canBuy == 0){
+            take = -arr[index] + solv(arr, index + 1, 1, tx);
+            notTake = solv(arr, index + 1, 0, tx);
+        }
+        else{
+            take = arr[index] + solv(arr, index + 1, 0, tx - 1);
+            notTake = solv(arr, index + 1, 1, tx);
+        }
+
+        return Math.max(take, notTake);
+    }
+
+*/
 class Solution {
     public int maxProfit(int k, int[] arr) {
-        int[][][] dp = new int[arr.length + 1][2][k + 1];
-        
-        for(int index = arr.length - 1; index >= 0; index--){
-            for(int canBuy = 0 ; canBuy <=1 ; canBuy++){
-                for(int tx = 1; tx <=k ; tx++){
-                    int take = 0;
-                    int notTake = 0;
+        int[][][] dp = new int[arr.length][2][k+1];
 
-                    if(canBuy == 0){
-                      take = -arr[index] + dp[index + 1][1][tx];
-                      notTake = dp[index + 1][0][tx];
-                    }else{
-                      take = arr[index] + dp[index + 1][0][tx - 1];
-                      notTake = dp[index + 1][1][tx];
-                    }
-
-                  dp[index][canBuy][tx] = Math.max(take, notTake);
-                }
+        for (int index = 0; index < arr.length; index++) {
+            for (int canBuy = 0; canBuy <= 1; canBuy++) {
+                Arrays.fill(dp[index][canBuy], -1);
             }
         }
-        return dp[0][0][k];
+
+        return solv(arr, 0, 0, k, dp);
     }
 
     int solv(int[] arr, int index, int canBuy, int tx, int[][][] dp){
         
         if(tx == 0 || index == arr.length)
-            return 0;
+          return 0; 
 
         if(dp[index][canBuy][tx] != -1)
-            return dp[index][canBuy][tx];
+            return dp[index][canBuy][tx] ;
 
         int take = 0;
-        int notTake = 0;
+        int notTake= 0;
 
         if(canBuy == 0){
             take = -arr[index] + solv(arr, index + 1, 1, tx, dp);
             notTake = solv(arr, index + 1, 0, tx, dp);
-        }else{
+        }
+        else{
             take = arr[index] + solv(arr, index + 1, 0, tx - 1, dp);
             notTake = solv(arr, index + 1, 1, tx, dp);
         }
