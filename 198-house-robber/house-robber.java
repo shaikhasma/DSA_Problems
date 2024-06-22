cfa504f0d7d57e1ec98e9c2fe02de319ad817e8c
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-22 13:02:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (78.65%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,23 +1,22 @@
 class Solution {
     public int rob(int[] arr) {
         int n = arr.length;
+        
         int[] dp = new int[n];
-        Arrays.fill(dp, -1);
+        dp[0] = arr[0];
 
-        return solv(arr, n - 1, dp);
+        for( int index = 1; index < n; index++){
+
+            int take = arr[index];
+            if(index > 1)
+                take += dp[index - 2];
+            
+            int notTake = dp[index - 1];
+
+            dp[index] = Math.max(take, notTake);
+        }
+
+        return dp[n - 1];
     }
-    int solv(int[] arr, int index, int[] dp){
-        if(index < 0)
-            return 0;
-
-        if(index == 0)
-            return arr[0];
-        if(dp[index] != - 1)
-            return dp[index];
-
-        int pick = arr[index] + solv(arr, index - 2, dp);
-        int notPick = solv(arr, index - 1, dp);
-
-        return dp[index ] = Math.max(pick, notPick); 
-    }
+    
 }
