00493b82fdd5acd2d57c038159ca6d1e9bce4767
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-22 19:25:06 +0530

    Time: 1 ms (81.62%) | Memory: 41.5 MB (58.47%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,32 +1,27 @@
 class Solution {
     public int maxProfit(int[] arr) {
-        int[][] dp = new int[arr.length][2];
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
+        int[][] dp = new int[arr.length + 2][2];
+       
 
-        return solv(arr, 0, 0, dp);
-    }
+        for(int index = arr.length - 1; index >= 0; index--){
+            for(int canBuy = 0 ; canBuy < 2; canBuy++){
+               
+               int take = 0;
+               int notTake = 0;
+               
+               if(canBuy == 0 ){
+                take = -arr[index] + dp[index + 1][1];
+                notTake = dp[index + 1][0];
+               }
+               else{
+                take = arr[index] + dp[index + 2][0];
+                notTake = dp[index + 1][1];
+               }
 
-    int solv(int[] arr, int index, int canBuy, int[][] dp){
-
-        if(index >= arr.length)
-            return 0;
-
-        if(dp[index][canBuy] != -1)
-            return dp[index][canBuy];
-             
-        int take = 0;
-        int notTake = 0;
-
-        if(canBuy == 0){
-            take = -arr[index] + solv(arr, index + 1, 1, dp);
-            notTake = solv(arr, index + 1, 0, dp);
-        }
-        else{
-            take = arr[index] + solv(arr, index + 2, 0, dp);
-            notTake = solv(arr, index + 1, 1, dp);
+               dp[index][canBuy] = Math.max(take, notTake);
+            }
         }
 
-        return dp[index][canBuy] = Math.max(take, notTake);
+        return dp[0][0];
     }
 }
