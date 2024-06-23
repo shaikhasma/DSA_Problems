b3da4b3609ce5a7b5d6b9b04bec4802bd7436750
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-23 22:06:46 +0530

    Time: 1 ms (24.40%) | Memory: 41.3 MB (91.52%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -164,26 +164,33 @@ class Solution {
             return 0;
 
         int[][] dp = new int[n][m];
-        for(int[] row : dp)
+        for(int[] row : dp )
             Arrays.fill(row, -1);
 
-        return solv(grid, n - 1, m - 1 , dp );
-    }
-    int solv(int[][] grid, int row, int col, int[][] dp){
+        for(int row = 0; row < n ; row++){
+            for(int col = 0; col < m ; col++){
+                if(row == 0 && col == 0){
+                    dp[0][0] = 1;
+                    continue;
+                }
 
-        if(row < 0 || col < 0 || grid[row][col] == 1)
-            return 0;
+                if(grid[row][col] == 1){
+                    dp[row][col] = 0;
+                    continue;
+                }     
+                    
+                int up = 0;
+                if(row > 0)
+                    up =  dp[row - 1][col];
 
-        if(row == 0 && col == 0)
-            return 1;
-
-        if(dp[row][col] != -1)
-            return dp[row][col];
-
-        int up =  solv(grid, row - 1, col, dp);
+                int left = 0;
+                if(col > 0)
+                    left =  dp[row][col - 1];
         
-        int left = solv(grid, row, col - 1, dp);
-        
-        return dp[row][col] = up + left;       
+               dp[row][col] = up + left;   
+            }
+        }
+
+        return dp[n - 1][m - 1];
     }
 }
