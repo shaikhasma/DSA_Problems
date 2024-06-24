b91132d77ea4822f45c7832b345a113aff79f1de
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-24 19:24:20 +0530

    Time: 4 ms (23.33%) | Memory: 47 MB (73.36%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,56 +1,61 @@
 /*
-      0 1 2
-    0 1,1,0
-    1 1,1,0
-    2 0,0,1
+  1 2 3
+1[1,1,0],
+2[1,1,0],
+3[0,0,1]]
+1 - -- 2 
+V      V
 
-    0 -- 1
-    1 ---0
-    2. ---
-     0 1 2
-    [T T T]
+3 no path
+V
+
+2
+
+ //prepareGraph
+        //visited[]
+        // dfs 1 - row check non visited
+        // count dfs call
 */
 class Solution {
-    public int findCircleNum(int[][] graph) {
-        int component = 0;
-        ArrayList<ArrayList<Integer>> adjList = prepareAdjList(graph);
-        boolean[] visited = new boolean[graph.length];
+    public int findCircleNum(int[][] matrix) {
+       ArrayList<ArrayList<Integer>> adjList = prepareGraph(matrix);
+       boolean[] visited = new boolean[matrix.length];
+       int component = 0;
 
-        for( int node = 0 ; node < graph.length; node++){
-          if(!visited[node]){
-            component++;
-            dfs(node, adjList, visited);
-          }
-        }
+       for(int node = 0 ; node < matrix.length ; node++){
+            if(!visited[node]){
+                 component++;
+                 dfs(node, visited, adjList);
+            }
+       }
 
-        return component;
+       return component;
     }
-  
-    void dfs(int node, ArrayList<ArrayList<Integer>> adjList, boolean[] visited){
+
+    void dfs(int node, boolean[] visited, ArrayList<ArrayList<Integer>> adjList){
         visited[node] = true;
 
-        for( Integer neighbor : adjList.get(node)){
+        for(Integer neighbor : adjList.get(node)){
             if(!visited[neighbor]){
-                dfs(neighbor, adjList, visited);
+                dfs(neighbor, visited, adjList);
             }
         }
     }
 
-    ArrayList<ArrayList<Integer>> prepareAdjList(int[][] graph){
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+    ArrayList<ArrayList<Integer>> prepareGraph(int[][] matrix){
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
 
-        for(int index = 0; index < graph.length; index++){
-            list.add(new ArrayList<>());
+        for(int index = 0; index < matrix.length ; index++){
+            adjList.add(new ArrayList<>());
         }
-       
-        for(int row = 0; row < graph.length; row++){
-            for(int col = 0; col < graph[0].length; col++){
-                if(graph[row][col] == 1){
-                    list.get(row).add(col);
-                }
+
+        for(int row = 0; row < matrix.length; row++){
+             for(int col = 0; col < matrix[0].length; col++){
+                if(matrix[row][col] == 1)
+                   adjList.get(row).add(col);
             }
         }
 
-        return list;
+        return adjList;
     }
 }
