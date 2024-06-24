30e30466439360a207dc89da10130f92cf3a54e5
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-24 19:37:25 +0530

    Time: 3 ms (18.48%) | Memory: 42.6 MB (24.64%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,72 +2,64 @@ class Orange{
     int row;
     int col;
     int time;
-
     public Orange(int row, int col, int time){
         this.row = row;
         this.col = col;
         this.time = time;
+
     }
 }
 class Solution {
     public int orangesRotting(int[][] grid) {
-    int[][] visited = new int[grid.length][grid[0].length];
-    int freshOrangeCount = 0;
-    Queue<Orange> q = new LinkedList<>();
+     boolean[][] visited = new boolean[grid.length][grid[0].length];
+     Queue<Orange> q = new LinkedList<>();
+     int freshOrange = 0;
+     //Step 1- Add all rottan orange into Q mark it as visited
+    //         & maintain fresh orange count
+     for(int row = 0; row < grid.length; row ++){
+         for(int col = 0; col < grid[0].length; col++){
+             if(grid[row][col] == 2){
+                 visited[row][col] = true;
+                 q.add(new Orange(row,col,0));
+             }
+             else if(grid[row][col] == 1){
+                 freshOrange++;
+             }
+         }
+     }
 
-    //step 1 : traverse matrix 
-    //          push all rotten orange into Q
-    //          mark the rotten orange as visited in visited[][]
-    //          count freshOrange
+     //Step 2 : BFS for remaning orange count newly rotten orange
+     //         & maintain max time
+     int maxTime = 0;
+     int rottenCount = 0;
+     while(!q.isEmpty()){
+         Orange orange = q.poll();
+         int r = orange.row;
+         int c = orange.col;
+         int t = orange.time;
+         maxTime = Math.max(maxTime, t);
 
-    for(int row = 0; row < grid.length; row++){
-        for(int col = 0; col < grid[0].length; col++){
-            if(grid[row][col] == 2){
-                q.add(new Orange(row, col, 0));
-                visited[row][col] = 2;
-            }else if(grid[row][col] == 1){
-                freshOrangeCount ++;
-            }
-        }
-    }
-    
-    //Step 2:  take oranges from q one by one 
-    //         and rotten all four direction's oranges
-    //         push into Q with time + 1
-    //         mark in visited[][]
-    //         count rotten orange
+         // checking four direction fresh non visited orange and pushing into Q
+         int[] dRow = {0, -1, 0, 1};
+         int[] dCol = {-1, 0, 1, 0};
 
-   int totalTime = 0;
-   int rottenOrange = 0;
-    while(!q.isEmpty()){
-        Orange orange = q.remove();
-        int row = orange.row;
-        int col = orange.col;
-        int time = orange.time;
-        totalTime = Math.max(totalTime, time);
+         for(int index = 0; index <= 3; index++){
+             int row = r + dRow[index];
+             int col = c + dCol[index];
 
-        int[] dRow = {1, -1, 0, 0 };
-        int[] dCol = {0, 0, 1, -1}; 
+             if(row >= 0 && row < grid.length && col >= 0 && col < grid[0].length &&
+                visited[row][col] == false && grid[row][col] == 1){
+                    visited[row][col] = true;
+                    q.add(new Orange(row, col, t + 1));
+                    rottenCount ++;
+                }
+         }
+     }
 
-        for( int dirIndex = 0 ; dirIndex <=3 ; dirIndex++){
-            int nRow = row + dRow[dirIndex];
-            int nCol = col + dCol[dirIndex];
+     if(freshOrange != rottenCount){
+         return -1;
+     }
 
-            if(nRow >= 0 && nRow < grid.length &&
-               nCol >= 0 && nCol < grid[0].length &&
-               grid[nRow][nCol] == 1 && visited[nRow][nCol] != 2){
-                   q.add(new Orange(nRow, nCol, time + 1));
-                   visited[nRow][nCol] = 2;
-                   rottenOrange ++;
-               }
-        }
-    }
-
-    if(freshOrangeCount != rottenOrange){
-     return  -1;
-    }
-
-    return totalTime;
-
+     return maxTime;
     }
 }
