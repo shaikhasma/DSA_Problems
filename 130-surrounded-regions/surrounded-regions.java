23c63b589622c931c44d234a25d6247ba346ec0a
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-25 11:05:18 +0530

    Time: 2 ms (86.12%) | Memory: 45 MB (79.09%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -11,15 +11,6 @@
 ["X","X","X","X"],
 ["X","X","X","X"]
 */
-class Node{
-    int row;
-    int col;
-    public Node (int row, int col){
-        this.row = row;
-        this.col = col;
-    }
-}
-
 class Solution {
     public void solve(char[][] board) {
      int n = board.length;
@@ -28,71 +19,66 @@ class Solution {
      int firstRow = 0;
      int firstCol = 0;
      int lastRow = n - 1;
-     int lastCol = m - 1; 
-
+     int lastCol = m - 1;
+     
      boolean[][] visited = new boolean[board.length][board[0].length];
-
-     Queue<Node> q = new LinkedList<>();
-     //1st row
-     for(int index = 0; index < m; index++){
-        if(board[firstRow][index] =='O' && !visited[firstRow][index]){
-            q.add(new Node(firstRow,index));
-            visited[firstRow][index] = true;
+     
+     //1st Row
+     for(int col = 0 ; col < m ; col++){
+        if(board[firstRow][col] == 'O' && !visited[firstRow][col]){
+            visited[firstRow][col] = true;
+            dfs(board, firstRow, col, visited);
         }
      }
 
-     //last row
-     for(int index = 0; index < m ; index++){
-         if(board[lastRow][index] =='O' && !visited[lastRow][index]){
-            q.add(new Node(lastRow,index));
-            visited[lastRow][index] = true;
-         }
-     }
-
-     //1st Col
-     for(int index = 0; index < n; index++){
-         if(board[index][firstCol] =='O' && !visited[index][firstCol]){
-            q.add(new Node(index,firstCol));
-            visited[index][firstCol] = true;
-         }
-     }
-
-     //last col
-     for(int index = 0; index < n; index++){
-         if(board[index][lastCol] =='O' && !visited[index][lastCol]){
-            q.add(new Node(index,lastCol));
-            visited[index][lastCol] = true;
-         }
-     }
-
-
-    int[] dRow = {-1, 0 , 1, 0};
-    int[] dCol = {0, 1, 0 , -1};
-
-     while(!q.isEmpty()){
-        Node node = q.poll();
-        int row = node.row;
-        int col = node.col;
-      
-        for( int index = 0; index < 4; index++){
-           int nRow = row + dRow[index];
-           int nCol = col + dCol[index];
-
-           if(nRow >= 0 && nRow < n && nCol >=0 && nCol < m &&
-                !visited[nRow][nCol] && board[nRow][nCol] == 'O'){
-                q.add(new Node(nRow, nCol));
-                visited[nRow][nCol] = true;
-                }
+     //Last Row
+     for(int col = 0 ; col < m ; col++){
+        if(board[lastRow][col] == 'O' && !visited[lastRow][col]){
+            visited[lastRow][col] = true;
+            dfs(board, lastRow, col, visited);
         }
      }
 
-     for(int row = 0; row < n; row++){
-        for(int col = 0; col < m; col++){
-            if(board[row][col] == 'O' && visited[row][col] == false){
-                board[row][col] = 'X';
-            } 
+     //1st col
+     for(int row = 0 ; row < n ; row++){
+        if(board[row][firstCol] == 'O' && !visited[row][firstCol]){
+            visited[row][firstCol] = true;
+            dfs(board, row, firstCol, visited);
         }
      }
+
+        //last col
+        for(int row = 0 ; row < n ; row++){
+            if(board[row][lastCol] == 'O' && !visited[row][lastCol]){
+                visited[row][lastCol] = true;
+                dfs(board, row, lastCol, visited);
+            }
+        } 
+
+      //flip
+        for(int row = 0 ; row < n ; row++){
+            for(int col = 0; col < m ; col++){
+                if(board[row][col] == 'O' && !visited[row][col])
+                    board[row][col] = 'X';
+            }
+        }
 
     }
+
+    void dfs(char[][] board, int row, int col, boolean[][] visited){
+        visited[row][col] = true;
+            
+        // L ,T, R, D
+        int[] dRow = {0, -1, 0, 1};
+        int[] dCol = {-1, 0, 1, 0};
+        for(int index = 0; index <= 3; index++){
+            int nRow = row + dRow[index];
+            int nCol = col + dCol[index];
+
+            if(nRow >= 0 && nRow < board.length && nCol >= 0 && nCol < board[0].length && 
+                board[nRow][nCol] == 'O' && !visited[nRow][nCol])
+                dfs(board, nRow, nCol, visited);
+        }
+    }
+
 }
