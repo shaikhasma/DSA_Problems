70a0d5f9e68c29879e92fb89e3b1f0c5d0cf7772
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-25 10:36:21 +0530

    Time: 3 ms (33.66%) | Memory: 44.8 MB (93.84%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -11,58 +11,88 @@
 ["X","X","X","X"],
 ["X","X","X","X"]
 */
+class Node{
+    int row;
+    int col;
+    public Node (int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+}
+
 class Solution {
     public void solve(char[][] board) {
+     int n = board.length;
+     int m = board[0].length;
+
+     int firstRow = 0;
+     int firstCol = 0;
+     int lastRow = n - 1;
+     int lastCol = m - 1; 
+
      boolean[][] visited = new boolean[board.length][board[0].length];
 
-     for(int index = 0; index < board[0].length; index++){
-       //1st row
-       if(board[0][index] == 'O' && !visited[0][index]){
-          dfs(board, 0, index, visited);
-       }
-
-       // last row
-       if(board[board.length - 1][index] == 'O' && !visited[board.length - 1][index]){
-        dfs(board, board.length - 1, index, visited);
-       }
+     Queue<Node> q = new LinkedList<>();
+     //1st row
+     for(int index = 0; index < m; index++){
+        if(board[firstRow][index] =='O' && !visited[firstRow][index]){
+            q.add(new Node(firstRow,index));
+            visited[firstRow][index] = true;
+        }
      }
 
-     for(int index = 0; index < board.length; index++){
-        // 1st col
-        if(board[index][0] =='O' && !visited[index][0]){
-            dfs(board, index, 0, visited);
-        }
-
-
-        // last col
-         if(board[index][board[0].length - 1 ] == 'O' 
-               && !visited[index][board[0].length - 1] ){
-            dfs(board,  index, board[0].length - 1, visited);
+     //last row
+     for(int index = 0; index < m ; index++){
+         if(board[lastRow][index] =='O' && !visited[lastRow][index]){
+            q.add(new Node(lastRow,index));
+            visited[lastRow][index] = true;
          }
      }
 
-     // traverse to flip
-     for(int row = 0; row < board.length; row++){
-        for(int col = 0; col < board[0].length; col++){
-            if( board[row][col] == 'O' && !visited[row][col]){
-                board[row][col] ='X';
-            }
-        }
-     }   
-        
-    }
+     //1st Col
+     for(int index = 0; index < n; index++){
+         if(board[index][firstCol] =='O' && !visited[index][firstCol]){
+            q.add(new Node(index,firstCol));
+            visited[index][firstCol] = true;
+         }
+     }
 
-    void dfs(char[][] board, int row, int col, boolean[][] visited){
+     //last col
+     for(int index = 0; index < n; index++){
+         if(board[index][lastCol] =='O' && !visited[index][lastCol]){
+            q.add(new Node(index,lastCol));
+            visited[index][lastCol] = true;
+         }
+     }
+
+
+    int[] dRow = {-1, 0 , 1, 0};
+    int[] dCol = {0, 1, 0 , -1};
+
+     while(!q.isEmpty()){
+        Node node = q.poll();
+        int row = node.row;
+        int col = node.col;
       
+        for( int index = 0; index < 4; index++){
+           int nRow = row + dRow[index];
+           int nCol = col + dCol[index];
 
-        if(row >= 0  &&  row < board.length && col >= 0 && col < board[0].length &&
-            board[row][col] == 'O' && !visited[row][col]){
-          visited[row][col] = true;
-            dfs(board, row + 1, col, visited);
-            dfs(board, row , col - 1, visited);
-            dfs(board, row - 1, col, visited);
-            dfs(board, row, col + 1, visited);
+           if(nRow >= 0 && nRow < n && nCol >=0 && nCol < m &&
+                !visited[nRow][nCol] && board[nRow][nCol] == 'O'){
+                q.add(new Node(nRow, nCol));
+                visited[nRow][nCol] = true;
+                }
         }
+     }
+
+     for(int row = 0; row < n; row++){
+        for(int col = 0; col < m; col++){
+            if(board[row][col] == 'O' && visited[row][col] == false){
+                board[row][col] = 'X';
+            } 
+        }
+     }
 
     }
 }
