44d6428493c51aa2fcc28397511bdaae07cb4784
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-06-29 03:12:25 +0530

    Time: 59 ms (5.69%) | Memory: 45.8 MB (94.64%) - LeetSync

---
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -0,0 +1,30 @@
+/*
+  Approach - 1 Brute force + using hashing k integers
+
+  TC - 0(N)
+  SC- 0(K)
+
+*/
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int K) {
+        return atMostK(nums, K) - atMostK(nums, K - 1);
+    }
+
+    int atMostK(int[] A, int K) {
+        int i = 0, res = 0;
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int j = 0; j < A.length; j++) {
+            if (count.getOrDefault(A[j], 0) == 0) 
+                K--;
+
+            count.put(A[j], count.getOrDefault(A[j], 0) + 1);
+            while (K < 0) {
+                count.put(A[i], count.get(A[i]) - 1);
+                if (count.get(A[i]) == 0) K++;
+                i++;
+            }
+            res += j - i + 1;
+        }
+        return res;
+    }
+}
