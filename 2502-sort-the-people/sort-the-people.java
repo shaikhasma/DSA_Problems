6e145b50f936760ed8455993f52a4654362bda91
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-07-22 14:04:15 +0530

    Time: 7 ms (90.64%) | Memory: 45.6 MB (5.32%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.java
@@ -0,0 +1,42 @@
+/*
+    ["Mary","John","Emma"]
+    [180,165,170]
+*/
+
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        HashMap<Integer, String> map = new HashMap<>();
+
+        for(int index = 0 ; index < heights.length; index++)
+            map.put(heights[index], names[index]);
+
+        Arrays.sort(heights);
+        reverseArray(heights);
+    
+
+        String[] sortedPeople = new String[names.length];
+        int pos = 0;
+
+        for(int index = 0 ; index < heights.length; index++)
+          sortedPeople[pos++] = map.get(heights[index]);
+
+        return sortedPeople;
+        
+    }
+
+     public  void reverseArray(int[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        
+        while (left < right) {
+            // Swap the elements
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            
+            // Move the pointers
+            left++;
+            right--;
+        }
+    }
+}
