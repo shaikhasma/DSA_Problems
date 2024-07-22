19365bec83ff1bf52d63c52ba51f483108e8a845
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-07-22 14:08:13 +0530

    Time: 11 ms (42.70%) | Memory: 45.7 MB (5.32%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,45 +1,26 @@
 /*
     ["Mary","John","Emma"]
     [180,165,170]
-    TC - 0(logN) + 0(N) + 0(N)
+    TC - 0(logN) + 0(N)
         - N + logN
     SC - 0(1)
 */
 
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        HashMap<Integer, String> map = new HashMap<>();
+        String[] result = new String[names.length];
+      
+        TreeMap < Integer, String > map = new TreeMap < > (Collections.reverseOrder());
 
-        for(int index = 0 ; index < heights.length; index++)
-            map.put(heights[index], names[index]);
-
-        Arrays.sort(heights);
-        reverseArray(heights);
+           // Heights in descending order
+         for (int i = 0; i < names.length; i++) 
+             map.put(heights[i], names[i]);         
     
 
-        String[] sortedPeople = new String[names.length];
-        int pos = 0;
-
-        for(int index = 0 ; index < heights.length; index++)
-          sortedPeople[pos++] = map.get(heights[index]);
-
-        return sortedPeople;
+        int i = 0;
+        for (int h: map.keySet()) 
+             result[i++] = map.get(h);            
         
-    }
-
-     public  void reverseArray(int[] array) {
-        int left = 0;
-        int right = array.length - 1;
-        
-        while (left < right) {
-            // Swap the elements
-            int temp = array[left];
-            array[left] = array[right];
-            array[right] = temp;
-            
-            // Move the pointers
-            left++;
-            right--;
-        }
+        return result;
     }
 }
