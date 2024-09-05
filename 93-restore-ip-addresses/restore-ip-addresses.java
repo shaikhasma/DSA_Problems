12417fbc8194f5ecc45f95cc5cf57f55cc8feb51
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2024-09-05 06:00:01 +0530

    Time: 6 ms (37.51%) | Memory: 43.6 MB (19.69%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> ans = new ArrayList<String>();
+
+        if(s.length() > 12)
+           return ans;
+
+        String str = s;
+        validIp(str, "", 0, 0, ans);
+        return ans;
+    }
+
+    void validIp(String str, String path, int index, int dots, List<String> ans){
+        if(dots > 4) 
+            return;
+        if(dots == 4 && index >= str.length()){
+            ans.add(path.substring(0, path.length() - 1));
+            return;
+        }
+
+        for(int length = 1; length <= 3 && index + length <= str.length(); length++ ){
+            
+            String num = str.substring(index, index + length);
+            
+            if( num.charAt(0) == '0' && length != 1 ){
+                break;
+            }     
+            else if(Integer.parseInt(num) <= 255){
+                validIp(str, 
+                path + str.substring(index, index + length) +"." , 
+                index + length, 
+                dots+ 1, 
+                ans );
+            }
+        }
+    }
+}
