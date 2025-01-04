86d60903805e3e620de692901d4b41f2f6ecc7eb
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-01-04 19:32:24 +0530

    Time: 285 ms (21.46%) | Memory: 45.8 MB (27.75%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,5 +1,37 @@
 /*
- Appraoch - find all unique chars + traverse unique chars get each unique char first and last index 
+
+Approach - 1 Traverse a to z chars 
+
+TC : O(26) * N
+indexOf and lastIndexOf: 0(N) worst Case
+Extract substring and calculate distinct characters → O(n) per letter in the worst case.
+   0(N)
+    public int countPalindromicSubsequence(String s) {
+      
+        int res = 0;
+        
+        // Iterate over all possible characters ('a' to 'z')
+        for (char c = 'a'; c <= 'z'; c++) {
+            // Find the first and last occurrence of the current character
+            int start = s.indexOf(c);
+            int end = s.lastIndexOf(c);
+            
+            // If there is a valid range between first and last occurrence
+            if (start != -1 && end != -1 && start < end) {
+                // Use a set to track unique characters between start and end
+                res += s.substring(start + 1, end)
+                        .chars()
+                        .distinct()
+                        .count();
+            }
+        }
+    
+        return res;
+    }
+
+SC - 0(N) unique chars beween 1st and last chars
+------------------------------------------------------------------------------------------------------
+ Appraoch - 2  find all unique chars + traverse unique chars get each unique char first and last index 
  1. Find all unique char of string and store into set
  2. traverse all unique chars 
    - get first index of char
@@ -8,12 +40,13 @@
    - how many unique chars are there so we can find 3 size palindrome string
    - count unique chars size and add into main ans
 
-TC - 0(N)
-SC - 0(N)
+TC - unqiue chars 0(26) , traverse 0(26) , traverse between 1st and last index 0(N)
+SC - count unique 0(26)
 
-*/
-class Solution {
-    public int countPalindromicSubsequence(String s) {
+TC - 0(N)
+SC - 0(1)
+
+   public int countPalindromicSubsequence(String s) {
         int ans = 0;
         Set<Character> uniqueChars = new HashSet<>();
 
@@ -37,4 +70,30 @@ class Solution {
 
         return ans;
     }
+
+*/
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+      
+        int res = 0;
+        
+        // Iterate over all possible characters ('a' to 'z')
+        for (char c = 'a'; c <= 'z'; c++) {
+            // Find the first and last occurrence of the current character
+            int start = s.indexOf(c);
+            int end = s.lastIndexOf(c);
+            
+            // If there is a valid range between first and last occurrence
+            if (start != -1 && end != -1 && start < end) {
+                // Use a set to track unique characters between start and end
+                res += s.substring(start + 1, end)
+                        .chars()
+                        .distinct()
+                        .count();
+            }
+        }
+    
+        return res;
+    }
+
 }
