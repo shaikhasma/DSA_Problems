780421ad212f2446ab750fcdb948d6bde7fe5e28
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-01-06 20:39:57 +0530

    Time: 3 ms (94.79%) | Memory: 45.1 MB (20.83%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,4 +1,17 @@
-/*
+/* 
+Approach1  - using prefix + suffix array
+1. prefixCount and prefixSum == left to right traversal  
+2. suffixCount and suffixSum == right to left traversal  
+
+   - prefixCount counts how many balls are on left side boxes vice versa for suffixCount
+   - prefixSum sum/ frequency of all balss on left side vice versa for suffixSum
+3. Total operations for each box are the sum of operations from the prerfix and suffix traversal
+TC - 0(N) + 0(N)
+     0 (N)
+SC - 0(N) + 0(N)
+     0 (N)
+
+
  "move" balls from left to right, and track how many ops 
   we count how many balls we got so far in cnt
   accumulate it in ops
@@ -7,9 +20,8 @@
      - 0(N)\
     
   SC - 0(1)
-*/
-class Solution {
-    public int[] minOperations(String boxes) {
+
+ public int[] minOperations(String boxes) {
         int[] res = new int[boxes.length()];
         
         for (int i = 0, ops = 0, cnt = 0; i < boxes.length(); ++i) {
@@ -25,5 +37,36 @@ class Solution {
         }
 
         return res;
+    }
+  
+*/
+class Solution {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int[] distances = new int[n];
+
+        int prefixCount = 0;
+        int prefixSum = 0;
+
+        for (int i = 0; i < n; ++i) {
+            distances[i] = prefixCount * i - prefixSum;
+            if (boxes.charAt(i) == '1') {
+                ++prefixCount;
+                prefixSum += i;
+            }
+        }
+
+        int suffixCount = 0;
+        int suffixSum = 0;
+
+        for (int i = n - 1; i >= 0; --i) {
+            distances[i] += suffixSum - suffixCount * i;
+            if (boxes.charAt(i) == '1') {
+                ++suffixCount;
+                suffixSum += i;
+            }
+        }
+
+        return distances;
     }
 }
