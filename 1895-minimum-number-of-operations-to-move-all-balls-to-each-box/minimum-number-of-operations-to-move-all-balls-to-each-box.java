43dc5eea12185ebf0255b57f71e01c9c1608fe66
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-01-06 20:40:58 +0530

    Time: 4 ms (76.56%) | Memory: 44.8 MB (46.88%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -11,6 +11,36 @@ TC - 0(N) + 0(N)
 SC - 0(N) + 0(N)
      0 (N)
 
+   public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int[] distances = new int[n];
+
+        int prefixCount = 0;
+        int prefixSum = 0;
+
+        for (int i = 0; i < n; ++i) {
+            distances[i] = prefixCount * i - prefixSum;
+            if (boxes.charAt(i) == '1') {
+                ++prefixCount;
+                prefixSum += i;
+            }
+        }
+
+        int suffixCount = 0;
+        int suffixSum = 0;
+
+        for (int i = n - 1; i >= 0; --i) {
+            distances[i] += suffixSum - suffixCount * i;
+            if (boxes.charAt(i) == '1') {
+                ++suffixCount;
+                suffixSum += i;
+            }
+        }
+
+        return distances;
+    }
+-------------------------------------------------------------------------------------------------------------------------
+Approach 2 - CarryFoword + left to right & right t left traversal
 
  "move" balls from left to right, and track how many ops 
   we count how many balls we got so far in cnt
