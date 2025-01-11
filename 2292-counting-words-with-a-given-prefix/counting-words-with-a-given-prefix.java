59511f1f5fe98a8ffbabe570df414e4f6ed6323c
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-01-11 22:03:09 +0530

    Time: 10 ms (5.66%) | Memory: 45.8 MB (5.12%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -1,11 +1,44 @@
-class Solution {
-    public int prefixCount(String[] words, String pref) {
-        int count = 0;
-        for (String word : words) {
-            if (word.startsWith(pref)) {
-                count++;
-            }
+import java.util.HashMap;
+
+class TrieNode {
+    HashMap<Character, TrieNode> dict = new HashMap<>();
+    int startCount = 0;
+}
+
+class Trie {
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void add(String word) {
+        TrieNode current = root;
+        for (char c : word.toCharArray()) {
+            current.dict.putIfAbsent(c, new TrieNode());
+            current = current.dict.get(c);
+            current.startCount++;
         }
-        return count; 
+    }
+
+    public int startsWith(String prefix) {
+        TrieNode current = root;
+        for (char c : prefix.toCharArray()) {
+            if (!current.dict.containsKey(c)) {
+                return 0;
+            }
+            current = current.dict.get(c);
+        }
+        return current.startCount;
+    }
+}
+
+public class Solution {
+    public int prefixCount(String[] words, String pref) {
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.add(word);
+        }
+        return trie.startsWith(pref);
     }
 }
