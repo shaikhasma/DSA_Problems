cf915dd8396daa6f2884e3692a1172ae42b6f9c4
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-01-17 21:04:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (48.40%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,32 +1,16 @@
-/* 
-Search in 2 D Matrix selecting / skiping row/col  By Asma 
-T.C - 0(N)
-S.C - 0(1)
-*/
-
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-       int totalRow = matrix.length;
-       int totalCol = matrix[0].length;
-       int low = 0;
-       int high = totalRow * totalCol - 1;
+        int col = matrix[0].length - 1; 
+        int row = 0;
+        while(col >= 0 && row <= matrix.length - 1){
+           if(target == matrix[row][col])
+                return true;
+           else if(target < matrix[row][col] )
+               col--;
+           else 
+              row++;
+        }
 
-       while(low <= high){
-           int mid = low + ( high - low) / 2;
-
-           int row = mid / totalCol; 
-           int col = mid % totalCol;
-
-           if(matrix[row][col] == target){
-               return true;
-           }
-           if(matrix[row][col] < target){
-               low = mid + 1;
-           }else{
-               high = mid - 1;
-           }
-       }
-
-       return false;
-    }        
+        return false;
+    }
 }
