471ad45c7c57017f412afeffe0f492b77feec8d7
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-01-19 21:53:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (98.39%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -9,33 +9,25 @@
 TC- 0(NM)
 SC- 0(1)
 */
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-     if(nums1.length == 0 || nums2.length == 0)
-        return;
 
-     for(int index = 0; index < m; index++){
-        if(nums1[index] > nums2[0]){
-            swap(nums1, index, nums2, 0);
+class Solution {
+    public void merge(int[] nums1, int n, int[] nums2, int m) {
+        int i = n - 1;  // Last element in nums1's actual data
+        int j = m - 1;  // Last element in nums2
+        int k = n + m - 1;  // Last position in nums1
+
+        // Merge in reverse order
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
         }
 
-        int first = nums2[0];
-        int j = 1;
-        for(; j < nums2.length && nums2[j] < first ; j++)
-            nums2[j - 1] = nums2[j];
-        
-        nums2[j - 1] = first;
-     }
-      
-        int pos = m;
-        for(int index = 0 ; index < n; index++)
-            nums1[pos++] = nums2[index];
-
-    }
-
-    void swap(int[] A, int index1, int[] B, int index2){
-        int temp = A[index1];
-        A[index1] = B[index2];
-        B[index2] = temp;
+        // If there are elements left in nums2, copy them
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
     }
 }
