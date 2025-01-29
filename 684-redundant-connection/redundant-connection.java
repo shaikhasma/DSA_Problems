7f89e5c2eb3100ea65d4631a295d9798e205b37c
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-01-29 21:39:26 +0530

    Time: 1 ms (75.21%) | Memory: 43.1 MB (86.45%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,26 +1,45 @@
-class Solution {
-    private int[] parent;
+/*
+  Disjoint set
+  Time Complexity: O(N)length of edges
+  Space Complexity: O(N) for par and the recursion stack
+*/
+class DisjointSet{
+    int[] parent;
+    public DisjointSet(int size){
+        parent = new int[size + 1];
 
-    private int find(int x) {
-        if (x != parent[x])
-            parent[x] = find(parent[x]);
-
-        return parent[x];
-    }
-    private void union(int x, int y) {
-        parent[find(y)] = find(x);
-    }
-
-    public int[] findRedundantConnection(int[][] edges) {
-        parent = new int[edges.length+1];
-        for (int index = 0; index < parent.length; index++)
+        for(int index = 0; index < size ; index++)
             parent[index] = index;
+    }
 
+    public int find(int x){
+        if(x == parent[x])
+            return x;
+
+        //memorizing before return the answer
+        parent[x] = find(parent[x]);
+        return parent[x];   
+    }
+
+    public void union(int x, int y){
+        //set x
+        int parentX = find(x);
+        int parentY = find(y);
+
+        parent[parentX] = parentY;
+    }
+}
+class Solution {
+    
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        DisjointSet set = new DisjointSet(n);
+        
         for (int[] edge : edges)
-            if (find(edge[0]) == find(edge[1])) 
+            if (set.find(edge[0]) == set.find(edge[1])) 
                 return edge;
             else 
-                union(edge[0],edge[1]);
+                set.union(edge[0],edge[1]);
         return edges[0];
     }
 }
