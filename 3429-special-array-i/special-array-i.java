f6ef644851f627ca9e60ff0f5ddac49fb32bdefb
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-02-02 04:57:30 +0530

    Time: 1 ms (37.43%) | Memory: 43.8 MB (8.84%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -6,9 +6,10 @@ class Solution {
         for(int index = 1; index < nums.length; index++){
             int prev = nums[index - 1];
             int current = nums[index] ;
-            
-            if(!( prev % 2 == 1 && current % 2 == 0) && !( prev % 2 == 0 && current % 2 == 1) )
-                return false;       
+            //current with previous is same parity or not
+            if (current % 2 == prev % 2) {
+                return false;
+            }   
         }
 
         return true;
