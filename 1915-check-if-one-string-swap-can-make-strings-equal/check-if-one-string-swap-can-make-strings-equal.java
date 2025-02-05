26eade5b29cf47511a9c29e7b58a76568ae37c89
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-02-05 20:56:13 +0530

    Time: 1 ms (52.21%) | Memory: 41.6 MB (58.39%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -4,23 +4,17 @@ kanb
 */
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-       int i=-1, j=-1;
-        int cnt=0;
+       List<Integer> l = new ArrayList<>();
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) 
+                l.add(i);
 
-        for(int k=0; k<s1.length(); k++){
-            if(s1.charAt(k)!=s2.charAt(k)){
-                cnt++;
-                if(i==-1) i=k;
-                else if(j==-1) j=k;
-            }
+			if (l.size() > 2) 
+                return false; // added this line to short circuit the loop
         }
 
-        if(cnt==0) return true;
-        else if(cnt==2 && s1.charAt(i)==s2.charAt(j) && s1.charAt(j)==s2.charAt(i)){
-            return true;
-        }
-
-
-        return false;
+        return l.size() == 0 || (l.size() == 2
+                                 && s1.charAt(l.get(0)) == s2.charAt(l.get(1))
+                                 && s1.charAt(l.get(1)) == s2.charAt(l.get(0)));
     }
 }
