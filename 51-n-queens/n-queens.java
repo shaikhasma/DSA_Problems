b44b5c1f5cdaaf5ddbe7696c79a9d5ecb0a36455
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-02-16 02:35:46 +0530

    Time: 4 ms (39.90%) | Memory: 45 MB (38.51%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,137 +1,88 @@
-/*
-["..Q.",
-"Q...",
-"...Q",
-".Q.."
-],
-["...Q",
-"Q...",
-"..Q.",
-".Q.."
-],
-[".Q..",
-"...Q",
-"Q...",
-"..Q."
-],
-["...Q",
-".Q..",
-"Q...",
-"..Q."],
-
-[".Q..",
-"...Q",
-"..Q.",
-"Q..."],
-["..Q.",
-".Q..",
-"...Q",
-"Q..."],
-["...Q",
-"..Q.",
-".Q..",
-"Q..."
-]
-*/
 class Solution {
-    private static final char EMPTY_CELL = '.';
-    private static final char QUEEN = 'Q';
-    
+    final static char EMPTY = '.';
+    final static char QUEEN = 'Q';
+
     public List<List<String>> solveNQueens(int n) {
-       List<List<String>> list = new ArrayList<>();
-       char[][] board = newBoard(n);
-
-       fillQueen(board, 0, list);
-
-       return list;
+        char[][] board = createBoard(n);
+        List<List<String>> placements = new ArrayList<>();
+        
+        //col
+        solve(board, 0, placements);
+        return placements;
     }
 
-    void fillQueen(char[][] board, int col, List<List<String>> list){
-        
-        if(col == board[0].length){
-            list.add(boardToList(board));
+    void solve(char[][] board,int col, List<List<String>> ans){
+        //base 
+        if(col >= board[0].length){
+            ans.add(readBoardPlacements(board));
             return;
         }
-
-        for( int row = 0 ; row < board.length; row++){
-            if(check(board, row, col)){
-                board[row][col] = QUEEN;
-
-                fillQueen(board, col + 1, list);
-
-                board[row][col] = EMPTY_CELL;
+        
+        for(int row = 0; row < board.length ; row++){
+            if(isValidPlace(board, row, col)){
+               board[row][col] = QUEEN; //placment
+               solve(board, col + 1, ans );
+               board[row][col] = EMPTY;//backtrack
             }
         }
     }
+
+    boolean isValidPlace(char[][] board, int row, int col){
+      int tempRow = row;
+      int tempCol = col;
     
-    ArrayList<String> boardToList(char[][] board){
-        ArrayList<String> list = new ArrayList<String>();
+      // left side
+      while(tempCol >= 0){
+        if(board[tempRow][tempCol] == QUEEN)
+            return false;
+        tempCol--;
+      }
+       
+       tempRow = row;
+       tempCol = col;
+      //upper Left diagonal
+      while(tempRow >= 0 && tempCol >= 0){
+        if(board[tempRow][tempCol] == QUEEN)
+            return false;
+        tempCol--;
+        tempRow--;
+      }
+      
+      // lower Left diagonal
+       tempRow = row;
+       tempCol = col;
+        while(tempRow < board.length && tempCol >= 0){
+         if(board[tempRow][tempCol] == QUEEN)
+            return false;
 
-        for(int index = 0; index < board.length; index++){
-            String str = new String(board[index]);
-            list.add(str);
-        }
+        tempCol--;
+        tempRow++;
+      }
 
-        return list;
-
-    }
-    boolean check(char[][] board, int row, int col){
-        //left check
-        int tempRow = row;
-        int tempCol = col;
-
-        while(tempCol >= 0){
-            if( board[tempRow][tempCol] == QUEEN ) 
-                return false;
-
-            tempCol--;
-        }
-
-        //top check
-        tempRow = row;
-        tempCol = col;
-
-        while(tempRow >= 0){
-            if( board[tempRow][tempCol] == QUEEN ) 
-                return false;
-
-            tempRow--;
-        }
-
-        //left uppper Diagonal
-        tempRow = row;
-        tempCol = col;
-
-        while(tempCol >= 0 && tempRow >= 0){
-            if( board[tempRow][tempCol] == QUEEN ) 
-                return false;
-                
-            tempCol--;
-            tempRow--;
-        }
-
-         //left lower Diagonal
-        tempRow = row;
-        tempCol = col;
-
-        while(tempCol >= 0 && tempRow < board.length){
-            if( board[tempRow][tempCol] == QUEEN ) 
-                return false;
-            
-            tempCol--;
-            tempRow++;
-        }
-
-        return true;
+      return true;
     }
 
-    char[][] newBoard(int n){
-       char[][] board = new char[n][n];
-       
-       for(int index = 0; index < n; index++){
-         Arrays.fill(board[index], EMPTY_CELL);
-       }
-       
-       return board;
+    List<String> readBoardPlacements(char[][] board){
+        List<String> list = new ArrayList<String>();
+
+     for(int row = 0; row < board.length; row++){
+        StringBuilder str = new StringBuilder();
+        
+        for(int col = 0; col < board[0].length; col++){
+          str.append(board[row][col]+"");
+        }
+
+        list.add(str.toString());
+     }
+     return list;
+    }
+
+    char[][] createBoard(int n){
+        char[][] board = new char[n][n];
+        for(int index = 0; index < n; index++){
+            Arrays.fill(board[index], EMPTY);
+        }
+
+        return board;
     }
 }
