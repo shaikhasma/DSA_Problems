392b7efcf160ff2064c01ac6fe6091d3399ce3f4
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-02-21 11:48:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (16.92%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -8,73 +8,47 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- /* Approach 3 - Convert into circular LL
- 1. Traverse LL and calculate length
- 2. if k == length return head as it is
- 3. if k > length k = k % len;
-    
- 4. find breakPoint len - k need to traverse breakPoint - 1 
-    -- create breakPoint - 1 next element as head
-    -- break the link
-    
-5.  Return the new head
-
-1-- > 2 --> 3 ---> 4---> 5  k = 7
-           
-len = 5
-7>5
-k = 7 % 5 
-k = 2
-if k == len return head
-convert it into circular
-1--- > 2 ---> 3 --->4---> 5 ---> 1 ---> 2......
-breakPoint = 5 - 2
-traverse breakPoint
-
-1.    2.    3   h
-1-- > 2 --> 3   4---> 5---> 1--- > 2 ---> 3  
-                      
-
- TC : 0(n) + 0(n)
-      size    traverse till breakpoint
-      = 0(n)
-SC - 0(1)
-
- */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next == null || k == 0) 
-          return head;
-       
-       int len = 0;
-       ListNode temp = head;
-       while(temp.next != null){
-           len++;
-           temp = temp.next;
-       }
-       //convert linear to circutlar if k not equal to length
-        if( k == len + 1) 
+       if(head == null || head.next == null)
+            return head;
+       int size = getLLSize(head);
+      
+       if(k > size)
+           k = k % size;
+       if(size == k  || k == 0)
            return head;
 
-       //calculate last node in length and pointing last node to head    
-       temp.next = head;
-       len++;
+       ListNode first = head;
+       ListNode second = head;
+     
+       for(int count = 1; count <= k; count++){
+         second = second.next;
+       }
+       //second move to till last 
+       while(second != null && second.next != null){
+        first = first.next;
+        second = second.next;
+       }
+       //first at k - 1 th 
+       ListNode newHead = first.next;
+       first.next = null;
 
-        if(k > len)
-           k = k % len;
-          
-        // identify breaking point
-        int breakLinkPoint = len - k;
-        temp = head;
-        for(int index = 1; index < breakLinkPoint; index++ ){
-            temp = temp.next;
-        }
-        
-        head = temp.next;
-        temp.next = null;
+       second.next = head;
+       head = newHead;
 
-        return head;
-    
+       return head;
+       
     }
-    
+
+    int getLLSize(ListNode head){
+        int count = 0;
+        while(head != null){
+            count++;
+            head = head.next;
+        }
+
+        return count;
+
+    }
 }
