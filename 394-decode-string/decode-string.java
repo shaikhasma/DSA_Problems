d4786ff3b9af633abcd2975028c1e18b1f50f9f6
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-02-22 18:26:22 +0530

    Time: 1 ms (74.33%) | Memory: 41.5 MB (69.03%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,10 +1,16 @@
+/*
+ String traversal (N)
+ appending N * K
+ TC - 0(NK)
+ SC - 0(NK)
+ */
 class Solution {
     
     public String decodeString(String s) {
         Stack<Integer> numStack = new Stack<Integer>();      
         Stack<String> stringStack = new Stack<String>();
         int number = 0;
-        String ans = "";
+        StringBuilder ans = new StringBuilder();
 
         for(char ch : s.toCharArray()){
             if(Character.isDigit(ch)){
@@ -21,25 +27,27 @@ class Solution {
             }
             else{
                    
-                   String str = "";
+                   StringBuilder temp =  new StringBuilder();
                    while(!stringStack.peek().equals("["))
-                      str = stringStack.pop() + str;
+                      temp.insert(0, stringStack.pop());
                    
                    stringStack.pop();// remove [ bracket
+                   
                    //multiply string with numStack value
                    int count = numStack.pop();
-                   StringBuilder sb = new StringBuilder(str);
+                   StringBuilder sb = new StringBuilder();
 
-                   while(count > 1){
-                     sb.append(str);
+                   while(count >= 1){
+                     sb.append(temp);
                      count--;
                    }
+
                    stringStack.push(sb.toString());
             }
         }
            while(!stringStack.isEmpty()){
-            ans = stringStack.pop() + ans;
+            ans.insert(0, stringStack.pop());
            }
-            return ans;
+            return ans.toString();
     }
 }
