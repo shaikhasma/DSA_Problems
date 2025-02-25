b5c52c0bda4c10d217ae910871dff9af2a235277
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-02-25 05:59:35 +0530

    Time: 28 ms (15.82%) | Memory: 46.1 MB (7.50%) - LeetSync

---
+++ 829-subdomain-visit-count/subdomain-visit-count.java
@@ -0,0 +1,39 @@
+/*
+N - number of domain
+M - Average length domain 
+Travers N domain 
+  - split M domain
+  
+TC - 0(NM)
+SC - 0(NM) subdomain entry in hashmap
+
+*/
+class Solution {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> counts = new HashMap<>();
+
+        for(String domain : cpdomains){
+            //split the string using " " to separate the count and the domain.
+            String[] split1 = domain.split("\\s+");
+            
+            // Extract count
+            int freq = Integer.parseInt(split1[0]);
+
+            //Split the domain into parts based on ".".
+            String[] split2 = split1[1].split("\\.");
+            String current ="";
+
+            for( int index = split2.length - 1; index >= 0; index--){
+                current = split2[index] + ( index < split2.length - 1 ? "." : "") + current;
+                counts.put(current, counts.getOrDefault(current, 0) + freq);
+            }
+        }
+
+        List<String> ans = new ArrayList<>();
+        for(String domain: counts.keySet()){
+            ans.add(counts.get(domain) + " " + domain);
+        }
+
+        return ans;
+    }
+}
