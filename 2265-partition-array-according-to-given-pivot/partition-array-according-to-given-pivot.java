093972d35ec03331c1cedb0afb72aaca93b15543
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-03-04 10:45:43 +0530

    Time: 4 ms (100.00%) | Memory: 66.9 MB (58.31%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,32 +1,27 @@
 /*
- Approach - 1 using temp1 & temp2 array for less and greater elements
- TC. - 0(N)
- SC - 0(N)
+ Approach - 2 Using two Pointer
+ TC. - 0(N) worst case 0(N) + 0(N) if all are pivot
+ SC - 0(1)
+
 */
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> temp1 = new ArrayList<>();
-        List<Integer> temp2 = new ArrayList<>();
+        int[] ans = new int [nums.length];
+        int left = 0;
+        int right = nums.length - 1;
         
-        for(int index = 0; index < nums.length; index++){
-            if(nums[index] < pivot)
-                temp1.add(nums[index]);
-            else if(nums[index] > pivot)
-                temp2.add(nums[index]);
+        for( int i = 0, j = nums.length - 1; i < nums.length; i++, j--){
+            if(nums[i] < pivot)
+                ans[left++] = nums[i];
+            
+            if(nums[j] > pivot)
+                ans[right--] = nums[j];
         }
 
-        int totPivot = nums.length - ( temp1.size() + temp2.size() );
-        for( int index = 0; index < temp1.size(); index++)
-            nums[index] = temp1.get(index);
-
-        for( int index = temp1.size(); index < (temp1.size() + totPivot); index++)
-            nums[index] = pivot;
-
-        int idx = (temp1.size() + totPivot);
-        for( int index = 0 ; index < temp2.size(); index++)
-            nums[idx++] = temp2.get(index);
-
-
-        return nums;
+        //fill in between left & right with pivot
+        while(left <= right)
+            ans[left++]= pivot;
+        
+        return ans;
     }
 }
