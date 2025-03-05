0e17e3a465c13631c7a9165b0e12a74c717385de
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-03-05 19:45:00 +0530

    Time: 8 ms (44.42%) | Memory: 44.9 MB (21.92%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -2,32 +2,23 @@
  TC- 0(n^2) N diffrent state for each state n string
  SC - 0(N) stack , 0(M) set + 0(N)dp[] 
     - 0(2N + M)
+Approach - Bottom Up
+TC - 0(N^2)
+SC- 0(N) dp array
 */
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         HashSet < String> set = new HashSet<>(wordDict);
-        Boolean[] dp = new Boolean[s.length() + 1];
-        return solve(s, set, dp);
-    }
-
-    boolean solve(String s, HashSet<String> set, Boolean[] dp ){
-        if(s.isEmpty())
-            return true;
-
-        //using memoization
-        if(dp[s.length()] != null)
-            return dp[s.length()];
-
-        for(int index = 1; index <= s.length(); index++){
-            String subStr = s.substring(0, index);
-  
-            if(set.contains(subStr)){
-                if(solve(s.substring(index), set, dp)){
-                    return dp[s.length()] = true;
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;//base case or empty string always brekable
+        for(int i = 1; i <= s.length(); i++){
+            for( int j = 0; j < i; j++){
+                if(dp[j] && set.contains(s.substring(j, i))){
+                    dp[i] = true;
+                    break;
                 }
             }
         }
-
-        return dp[s.length()] = false;
+        return dp[s.length()];
     }
 }
