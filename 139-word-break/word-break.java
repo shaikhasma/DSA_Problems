4c7aeb5ccdaa98ebad38a68ebcfd2768f5a6562a
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-03-05 19:53:21 +0530

    Time: 6 ms (62.17%) | Memory: 45.1 MB (12.26%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -2,23 +2,54 @@
  TC- 0(n^2) N diffrent state for each state n string
  SC - 0(N) stack , 0(M) set + 0(N)dp[] 
     - 0(2N + M)
-Approach - Bottom Up
-TC - 0(N^2)
-SC- 0(N) dp array
+
+    here we are checking other part only if first part is valid 
+    applepenapple
+    1. call("appplepenapple")
+        here apple was valid so we call("penapple")
+    2. call("penapple")
+        here pen was call("apple")
+    3. call("apple")
+         here apple was valid call("")
+    4. call("") 
+       base case return true
+
+BackTrack - 
+   3rd  apple valid + T(base case)
+   
+   2nd  pen valid + T(apple)
+
+   1st apple + T ( penapple)
+
+   return true 
+   
+   we are not checking again pen even if it valid as we alredy checked this
 */
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         HashSet < String> set = new HashSet<>(wordDict);
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;//base case or empty string always brekable
-        for(int i = 1; i <= s.length(); i++){
-            for( int j = 0; j < i; j++){
-                if(dp[j] && set.contains(s.substring(j, i))){
-                    dp[i] = true;
-                    break;
+        Boolean[] dp = new Boolean[s.length() + 1];
+        return solve(s, set, dp);
+    }
+
+    boolean solve(String s, HashSet<String> set, Boolean[] dp ){
+        if(s.isEmpty())
+            return true;
+
+        //using memoization
+        if(dp[s.length()] != null)
+            return dp[s.length()];
+
+        for(int index = 1; index <= s.length(); index++){
+            String subStr = s.substring(0, index);
+  
+            if(set.contains(subStr)){
+                if(solve(s.substring(index), set, dp)){
+                    return dp[s.length()] = true;
                 }
             }
         }
-        return dp[s.length()];
+
+        return dp[s.length()] = false;
     }
 }
