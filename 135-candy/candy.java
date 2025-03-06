48a491bb4c97331f564ab407376f20b947a9b77c
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-03-06 11:50:17 +0530

    Time: 6 ms (14.01%) | Memory: 46 MB (43.68%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -6,8 +6,7 @@
   SC - 0(N) + 0(N)
        left.  right
     - 0(2N)
-*/
-class Solution {
+
     public int candy(int[] ratings) {
         int[] left = leftNeighbor(ratings);
         int[] right = rightNeighbor(ratings);
@@ -33,6 +32,54 @@ class Solution {
 
       return left;
     }
+
+    int[] rightNeighbor(int[] arr){
+      int[] right = new int[arr.length];
+      right[arr.length - 1] = 1;
+
+      for(int index = arr.length - 2; index >= 0; index--){
+          if(arr[index] > arr[index + 1])
+            right[index] = right[index + 1] + 1;
+          else
+            right[index] = 1;
+      }
+
+      return right;
+    }
+--------------------------------------------------------------------------------------------------------------------------
+Approach - 2 carry forward + right prefix
+TC - 0(2N)
+SC - 0(N)
+
+1. calcuate right distribution 
+2. carry left value and min candy distribution 
+3. every time carry left value 
+    max(left, right[index])
+    add into sum
+4. return sum
+
+
+*/
+class Solution {
+    public int candy(int[] ratings) {
+        int left = 1;
+        int[] right = rightNeighbor(ratings);
+        int minSum = Math.max(left, right[0]);
+
+        for(int index = 1; index < ratings.length; index++){
+            if(ratings[index - 1] < ratings[index])
+                left = left + 1;
+            else
+                left = 1;
+
+            minSum += Math.max(left, right[index]);
+        
+        }
+
+        return minSum;
+    }
+
+    
 
     int[] rightNeighbor(int[] arr){
       int[] right = new int[arr.length];
