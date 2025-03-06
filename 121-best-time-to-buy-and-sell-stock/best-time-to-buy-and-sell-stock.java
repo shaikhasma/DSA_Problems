2405af448d1db3ba090823b8263af75398882822
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-03-06 18:29:17 +0530

    Time: 5 ms (2.54%) | Memory: 60.5 MB (95.65%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,45 @@
-/** Approcah 2 Vally Approach
-TC - 0(n)
-SC - 0(1)
- */
+/*
+  [7,4,5,3,6,1] 
+
+  output  = 5
+*/
 class Solution {
-    public int maxProfit(int[] price) { 
-        int maxProfit = Integer.MIN_VALUE;
-        int minPrice = Integer.MAX_VALUE;
-      for(int index = 0 ; index < price.length ; index++){
-          minPrice = Math.min(minPrice, price[index]);
-          maxProfit = Math.max(maxProfit, price[index] - minPrice);
-      }
-      return maxProfit;
+    public int maxProfit(int[] arr) {
+       int[] leftMin = prefixMin(arr);
+       int[] rightMax = suffixMax(arr);
+       int maxProfit = 0;
+
+       for( int index = 0; index < arr.length ; index++)
+        maxProfit = Math.max(maxProfit , rightMax[index] - leftMin[index]);
+
+       return maxProfit;
+    }
+
+    int[] prefixMin(int[] arr){
+        int[] ans = new int[arr.length];
+        ans[0] = arr[0];
+
+        for( int index = 1; index < arr.length; index++){
+          if(ans[index - 1] > arr[index])
+            ans[index] = arr[index];
+          else
+            ans[index] = ans[index - 1];
+        }
+
+        return ans;
+    }
+
+    int[] suffixMax(int[] arr){
+        int[] ans = new int[arr.length];
+        ans[arr.length - 1] = arr[arr.length - 1];
+
+        for( int index = arr.length - 2; index >= 0 ; index--){
+          if(arr[index] > ans[index + 1])
+            ans[index] = arr[index];
+          else
+            ans[index] = ans[index + 1];
+        }
+
+        return ans;
     }
 }
