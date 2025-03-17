7103ecc1e00de1875315c1974544a1faf312281e
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-03-17 09:15:49 +0530

    Time: 732 ms (18.11%) | Memory: 41.5 MB (73.76%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,28 @@
+/*
+ Approach - Brute force
+ TC - 0(n^3)
+ SC - 0(1)
+*/
+class Solution {
+
+    public boolean isPalindrome(String s, int left, int right) {
+        while(left < right) {
+            if(s.charAt(left++) != s.charAt(right--)) 
+                 return false;
+        } 
+        return true;
+    }
+    
+    public int countSubstrings(String s) {
+        int ans = 0;
+        int n = s.length();
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+
+                if(isPalindrome(s, i, j)) 
+                    ans++;
+            }
+        }
+        return ans;
+    }
+}
