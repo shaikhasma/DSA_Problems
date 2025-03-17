674f92a4bf51cafe71275324332f5cff2a24edd3
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-03-17 07:50:23 +0530

    Time: 6 ms (61.93%) | Memory: 44.7 MB (29.73%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,55 +1,27 @@
-/*
- TC- 0(n^2) N diffrent state for each state n string
- SC - 0(N) stack , 0(M) set + 0(N)dp[] 
-    - 0(2N + M)
-
-    here we are checking other part only if first part is valid 
-    applepenapple
-    1. call("appplepenapple")
-        here apple was valid so we call("penapple")
-    2. call("penapple")
-        here pen was call("apple")
-    3. call("apple")
-         here apple was valid call("")
-    4. call("") 
-       base case return true
-
-BackTrack - 
-   3rd  apple valid + T(base case)
-   
-   2nd  pen valid + T(apple)
-
-   1st apple + T ( penapple)
-
-   return true 
-   
-   we are not checking again pen even if it valid as we alredy checked this
-*/
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        HashSet < String> set = new HashSet<>(wordDict);
+        HashSet<String> set = new HashSet<>();
         Boolean[] dp = new Boolean[s.length() + 1];
-        return solve(s, set, dp);
+        for(String word : wordDict)
+            set.add(word);
+
+        return solve(s,set, dp);
     }
 
-    boolean solve(String s, HashSet<String> set, Boolean[] dp ){
-        if(s.isEmpty())
+    boolean solve(String str,  HashSet<String> set, Boolean[] dp){
+        if(str.isEmpty())
             return true;
 
-        //using memoization
-        if(dp[s.length()] != null)
-            return dp[s.length()];
+        if(dp[str.length()] != null)
+            return dp[str.length()];
 
-        for(int index = 1; index <= s.length(); index++){
-            String subStr = s.substring(0, index);
-  
-            if(set.contains(subStr)){
-                if(solve(s.substring(index), set, dp)){
-                    return dp[s.length()] = true;
-                }
+        for(int index = 1 ; index <= str.length(); index++){
+            if(set.contains(str.substring(0,index))){
+                if(solve(str.substring(index), set, dp))   
+                    return dp[str.length()] = true;
             }
         }
 
-        return dp[s.length()] = false;
+        return dp[str.length()] = false;
     }
 }
