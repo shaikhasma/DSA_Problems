f56cf2e4f8b1c0a31d21fe587d60d1352b1803be
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-03-17 08:41:15 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (88.38%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,22 +1,28 @@
+/*.    Approach 1 - Recursion
+ TC - 0(2^N)
+ SC - 0(N) auxillary stack
+*/
 class Solution {
-    public int rob(int[] arr) {
-        int n = arr.length;
-        int prev = arr[0];
-        int prevOfPrev = 0;
+    public int rob(int[] nums) {
+      int[] dp = new int [nums.length + 1];
+      Arrays.fill(dp, -1);
 
-        for( int index = 1; index < n; index++){
-
-            int take = arr[index];
-            if(index > 1)
-                take += prevOfPrev;
-            
-            int notTake = prev;
-            int current = Math.max(take, notTake);
-            prevOfPrev = prev;
-            prev = current;
-        }
-
-        return prev;
+      return maxMoney(nums, nums.length - 1, dp);
     }
-    
+
+    int maxMoney(int[] house, int houseIndex, int[] dp){
+        if(houseIndex == 0) 
+            return house[houseIndex];
+
+       if(houseIndex < 0)
+           return 0;
+
+        if(dp[houseIndex] != -1)
+            return dp[houseIndex];
+
+        int pick = house[houseIndex] + maxMoney(house, houseIndex - 2, dp);
+        int notPick = maxMoney(house, houseIndex - 1, dp);
+
+        return dp[ houseIndex ] = Math.max(pick, notPick);
+    }
 }
