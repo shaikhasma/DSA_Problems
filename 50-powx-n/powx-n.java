64191624d8714487e4f3d8899dcd3a1689953b25
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2025-08-29 19:09:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (47.25%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,30 +1,27 @@
-/* Approach - 3 Binary Exponational + binary operator
-TC - 0(log2(N))
-SC - 0(1)
-*/
-
 class Solution {
     public double myPow(double x, int n) {
-        //if n is -ve
-        if(n < 0) {
-            n = -n;
-            x = 1 / x;
-        }
-        
-        double ans = 1;
-        while(n != 0){
-            //multiple only when number is oddodd number
-            if((n & 1) != 0){
-                ans = ans * x;
-            } 
-                
-            x *= x;
-            n >>>= 1; // n /= 2 or n = n >> 2
+        if(n == 0 || x == 1.0)
+            return 1;
+
+        long tempN = n; 
+        //if N negative make it +ve
+        if(n < 0){
+            x = 1/x;
+            tempN = -1L * n;
         }
 
-   
+        double ans = 1.0;
+        // N times multiplication
+        while( tempN > 0){
+            if(tempN % 2 == 1){
+               ans = ans * x;
+               tempN--;
+            }
+            
+            x = x * x;
+            tempN >>= 1;
+        }
 
-    return ans;
+        return ans;
     }
-    
 }
