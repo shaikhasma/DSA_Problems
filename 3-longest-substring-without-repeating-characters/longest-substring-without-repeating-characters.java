3bcce033f299a73078440a8c0e8ad7a1d61d2c69
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2026-03-22 19:32:35 +0530

    Time: 93 ms (5.96%) | Memory: 47.1 MB (8.74%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,22 +1,45 @@
+
+/* Approach 1 - Check all substring + hashing + two pointer
+
+1. Traverse all substring 
+2. once we find duplicate  calculate leng from start to end and stop 
+  and break that substring there it self
+3. Maintain max len
+4. Add character ( if character is already added it wont add again so add directly)
+TC- 0(N^2)
+SC - 0(N)
+
+"abcabcbb"
+
+
+*/
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        int len = 0;
-        int maxLen = 0;
-        int left = 0;
-        for(int index = 0; index < s.length(); index++){
-           char ch= s.charAt(index);
-           
-           if(map.containsKey(ch)){
-              left = Math.max(map.get(s.charAt(index)) + 1, left);
-           }
-          
-            map.put(ch,index);
-            len = index - left + 1;
-            maxLen = Math.max(len, maxLen);   
-        }
+       HashSet<Character> set = new HashSet<>();
+      int left = 0;
+      int right = 0;
+      int ans = 0;
+      int len = 0;
 
-        return maxLen;
+      while(right < s.length()){ 
+       Character ch = s.charAt(right);
 
+       if(!set.contains(ch)){
+        set.add(ch);
+        right++;
+        len++;
+       }
+       else{
+        left++;
+        right = left;
+        set = new HashSet<>();
+        len = 0;
+       }
+
+       ans = Math.max(ans, len);
+      }
+
+     return ans;
     }
 }
