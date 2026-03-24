b24e9c25b2c7e9538370f25dc9aabd26c28f0c34
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2026-03-25 00:15:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (19.47%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,25 +1,36 @@
+/*
+ Approach - 1 sort 
+ TC - 0(logN)
+ SC - 0(1)
+
+ Approach - 2 Count 0 & 1 & place it again as per coount
+ TC - 0(2N)
+ SC - 0(1)
+
+ Appraoch - 3 Two pointer appraoch 
+ TC - 0(N)
+ SC - (1)
+
+*/
 class Solution {
-    public void sortColors(int[] nums) {
+    public void sortColors(int[] arr) {
         int left = 0;
+        int right = arr.length - 1;
         int mid = 0;
-        int right = nums.length - 1;
 
         while(mid <= right){
-            if(nums[mid] == 0){
-                swap(nums, left, mid);
-                left++;
-                mid++;
-            }
-            else if(nums[mid] == 2){
-                swap(nums, right, mid);
+            if(arr[mid] == 0){
+               swap(arr, left, mid);
+               left++;
+               mid++;
+            }else if( arr[mid] == 2){
+                swap(arr, right, mid);
                 right--;
-            }
-            else{
+            }else{
                 mid++;
-            }
+            } 
         }
     }
-
     void swap(int[] arr, int left, int right){
         int temp = arr[left];
         arr[left] = arr[right];
