acaca7ff9dcbaaed9eea83a2066b40efaa81cc66
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2026-04-09 11:41:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (68.63%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -5,9 +5,6 @@ SC : 0(1)
 Appraoch- 2 Decide Row or Col traversal based on target value (Using properify of sorting row and col)
 TC : 0(max(n,m))
 SC : 0(1)
-
-*/
-class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
       int row = 0;
       int col = matrix[0].length - 1;
@@ -22,6 +19,33 @@ class Solution {
             col--;    
         
       }        
+      return false;
+    }
+
+Approach - 3 Binary Search
+
+
+*/
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+      int N = matrix.length;
+      int M = matrix[0].length; 
+
+      int low = 0;
+      int high = (N * M ) - 1;
+
+      while(low <= high){
+        int mid = low + (high - low) / 2;
+        int row = mid / M;
+        int col = mid % M;
+        if(matrix[row][col] == target)
+            return true;
+        
+        if(matrix[row][col] < target)
+            low = mid + 1;
+        else 
+            high = mid - 1;
+      }
       return false;
     }
 }
