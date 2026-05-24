73ae9409384f7d468d841ed0b12fdcfd56a7b648
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2026-05-24 20:26:10 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (13.89%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] arr, int target) {
+     int low = 0; 
+     int high = arr.length - 1;
+     
+     while(low <= high){
+        int mid = (low + high ) / 2;
+
+        if( arr[mid] == target)
+            return mid;
+
+        if( target > arr[mid])
+            low = mid + 1;
+        else
+            high = mid - 1;
+        
+     }
+     return -1;
+    }
+}
