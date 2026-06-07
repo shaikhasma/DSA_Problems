0d258022d5ba79b68b2b17b2ff0695e7d06796e4
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2026-06-07 19:50:19 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (72.51%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,24 +1,19 @@
-/* Approach - 1 Brute Force 
--- Traverse number 1 to max number of array element
-  --- if current element is less or equla then K
-      then 
-         shift k by one place
-      else
-         k is missing number
-
-TC - 0(N)
-SC - 0(1)
-*/
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-      for(int index = 0; index < arr.length; index++){
-       if( arr[index ] <= k) 
-           k++;
-       else
-         break;
-      }
-      return k;
-    }
+        int low = 0;
+        int high = arr.length - 1;
 
-    
+        while(low <= high){
+            int mid = (low + high)/2;
+            int missNo = arr[mid] - (mid + 1);
+
+            if(missNo < k)
+                low = mid + 1;
+            else
+                high = mid - 1;
+            
+        }
+
+        return high + 1 + k;//low + k
+    }
 }
