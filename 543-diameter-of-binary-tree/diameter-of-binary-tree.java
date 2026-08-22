34f0b53085d4847e5741905fec880940f1a33bc6
Asma Salim Shaikh <44858335+shaikhasma@users.noreply.github.com>
2026-08-22 10:46:26 +0530

    Time: 189 ms (7.65%) | Memory: 48.6 MB (5.29%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -13,33 +13,24 @@
  *     }
  * }
  */
-
- /*
-   Approach - postOrder + maintain max diameter (max carry diameter)
-
-   TC - 0(n) height of every node
-   SC -0 0(H) height of tree auxillary stack
- */
 class Solution {
-   //idea is carry max diamter so need reference    
-   public int diameterOfBinaryTree(TreeNode root) {
-    int[] diameter = new int[1];
+    public int diameterOfBinaryTree(TreeNode root) {
+        // base 
+        if(root == null)
+            return 0;
 
-    heightAndDiameter(root, diameter);
+        // core logic
+         int diameter = height(root.left) + height(root.right);
+        // recurcive call
+        int leftDia = diameterOfBinaryTree(root.left);
+         int rightDia = diameterOfBinaryTree(root.right);
+        return Math.max(diameter, Math.max(leftDia, rightDia));
+    }
 
-    return diameter[0];
-   }
-
-   int heightAndDiameter(TreeNode root, int[] diameter){
-    if(root == null)
-        return 0;
-    
-    int leftHeight = heightAndDiameter(root.left, diameter);
-    int rightHeight = heightAndDiameter(root.right, diameter);
-
-    diameter[0] = Math.max(diameter[0] , leftHeight + rightHeight); // maintain maxDaimeter
-    return Math.max(leftHeight, rightHeight) + 1; //height calculation
-
-   }
-
+    int height(TreeNode root){
+        if(root == null)
+            return 0;
+        
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
 }
